e3193b85eb5865c49f047f2bd3315ed0c9b39843
Sakshamdeep Singh <saksham.sds@gmail.com>
2022-04-10 23:43:18 +0530

    duplicate classes removed

--- leetcode/easy/MergeSortedLLists21.java
+++ leetcode/easy/MergeSortedLLists21.java
@@ -3,23 +3,6 @@ package easy;
 // https://leetcode.com/problems/merge-two-sorted-lists
 // 21. Merge Two Sorted Lists
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 public class MergeSortedLLists21 {
     public static void main(String[] args) {
 
