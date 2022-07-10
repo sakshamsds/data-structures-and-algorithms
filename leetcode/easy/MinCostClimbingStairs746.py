039daed2889c988bebdc81b1a3144b73f88f0da1
Saksham <saksham.sds@gmail.com>
2022-07-10 22:09:46 +0530

    Add daily leetcode solution

---
+++ leetcode/easy/MinCostClimbingStairs746.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        # calculate cost at every step
+        num_steps = len(cost)
+
+        for i in range(2, num_steps):
+            cost[i] += min(cost[i-1], cost[i-2])
+            print(cost)
+
+        return min(cost[num_steps-1], cost[num_steps-2])
+
+
+inst = Solution()
+print(inst.minCostClimbingStairs([10, 15, 20]))
+# print(inst.minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+print(inst.minCostClimbingStairs([0, 1, 2, 2]))
+
