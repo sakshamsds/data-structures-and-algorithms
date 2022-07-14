ce084b98ad75bb2672ed62fadda9f2953b884d1f
Saksham <saksham.sds@gmail.com>
2022-07-14 09:31:41 +0530

    Fix classpath

---
+++ ctci/ch_01_arrays_and_strings/PalindromePermutation.java
@@ -0,0 +1,11 @@
+package ch_01_arrays_and_strings;
+
+public class PalindromePermutation {
+    public static void main(String[] args) {
+        System.out.println(method("str"));
+    }
+
+    static boolean method(String str) {
+        return true;
+    }
+}
