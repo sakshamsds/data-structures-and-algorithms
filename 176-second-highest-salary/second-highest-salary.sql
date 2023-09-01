6bf0534b89ffa6670d8f969694bd3a1f382925ee
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-01 16:47:13 -0400

    Time: 422 ms (87.90%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,8 +1,19 @@
 # Write your MySQL query statement below
 
+# Solution 1
 # find max, then select highest which is not max
 
-SELECT MAX(salary) as SecondHighestSalary
-FROM employee
-WHERE salary < (SELECT MAX(salary) FROM employee);
-# WHERE salary NOT IN (SELECT MAX(salary) FROM employee);
+# SELECT MAX(salary) as SecondHighestSalary
+# FROM employee
+# WHERE salary < (SELECT MAX(salary) FROM employee);
+# # WHERE salary NOT IN (SELECT MAX(salary) FROM employee); # max return null by definition
+
+# Solution 2, fix for no entry by using subquery
+
+SELECT ( 
+  SELECT DISTINCT salary
+  FROM employee
+  ORDER BY salary DESC
+  LIMIT 1 OFFSET 1
+) as SecondHighestSalary;
+
