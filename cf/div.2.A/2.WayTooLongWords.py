a18b83d3c66ea88322c132cfd706dc7023de8d8a
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-01 16:13:04 -0400

    add windows update

--- cf/div.2.A/2.WayTooLongWords.py
+++
@@ -1,8 +0,0 @@
-
-num_words = int(input())
-for _ in range(num_words):
-    word =  input()
-    if len(word) <= 10:
-        print(word)
-    else:
-        print(f'{word[0]}{len(word) - 2}{word[-1]}')
--- cf/div.2.A/test.py
+++
@@ -1,5 +0,0 @@
-s = '2+3'
-num1 = '2'
-num2 = '3'
-op = '+'
-print(eval(num1 + op + num2))
