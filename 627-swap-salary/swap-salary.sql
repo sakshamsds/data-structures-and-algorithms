76fb6bf541a3b15e9a53ec660a318c16865bb33c
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-02 18:46:56 -0400

    Time: 443 ms (89.36%) | Memory: 0B (100.00%) - LeetSync

--- 627-swap-salary/swap-salary.sql
+++ 627-swap-salary/swap-salary.sql
@@ -1,8 +1,14 @@
 # Write your MySQL query statement below
 
 UPDATE salary
-SET sex = CASE 
-            WHEN sex = 'm'
-                THEN 'f'
-                ELSE 'm'
-            END;
+SET sex = CASE sex
+            WHEN 'm' THEN 'f'
+            ELSE 'm'
+          END;
+
+# UPDATE salary
+# SET sex = CASE 
+#             WHEN sex = 'm'
+#                 THEN 'f'
+#                 ELSE 'm'
+#             END;
