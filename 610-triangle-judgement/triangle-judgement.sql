8ee882ea370e77e76ac81e9a5131a04a68089437
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-02 18:22:51 -0400

    Time: 728 ms (18.96%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -6,10 +6,14 @@
 # c + a > b
 # if else -> CASE WHEN condition THEN a ELSE b in SQL
 
-SELECT 
-    *, 
-    CASE 
-        WHEN x+y>z AND y+z>x AND z+x>y THEN 'Yes' 
-        ELSE 'No' 
-    END as triangle
-FROM triangle
+# SELECT 
+#     *, 
+#     CASE 
+#         WHEN x+y>z AND y+z>x AND z+x>y THEN 'Yes' 
+#         ELSE 'No' 
+#     END as triangle
+# FROM triangle
+
+SELECT *,
+    IF (x+y>z AND y+z>x AND z+x>y, 'Yes', 'No') as triangle
+FROM Triangle;
