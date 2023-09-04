5087da13d65690bdf58d38fa30cbc04b866426a6
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-04 04:37:07 -0400

    Time: 2139 ms (20.56%) | Memory: 0B (100.00%) - LeetSync

--- 1734-bank-account-summary-ii/bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -1,12 +1,19 @@
 # Write your MySQL query statement below
 
-WITH cte AS
-(
-    SELECT u.name as NAME, SUM(t.amount) as BALANCE
-    FROM users u
-    INNER JOIN transactions t
-        ON u.account = t.account
-    GROUP BY u.account
-)
+# WITH cte AS
+# (
+#     SELECT u.name as NAME, SUM(t.amount) as BALANCE
+#     FROM users u
+#     INNER JOIN transactions t
+#         ON u.account = t.account
+#     GROUP BY u.account
+# )
 
-SELECT * FROM cte WHERE balance > 10000;
+# SELECT * FROM cte WHERE balance > 10000;
+
+SELECT u.name, SUM(t.amount) as balance
+FROM users u
+INNER JOIN transactions t
+    ON u.account = t.account
+GROUP BY u.account
+    HAVING balance > 10000;
