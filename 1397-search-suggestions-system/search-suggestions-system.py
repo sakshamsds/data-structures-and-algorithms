720f5e53800595e44b3c827a5c81654807fd1230
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-09 06:00:53 -0400

    Time: 851 ms (11.45%) | Memory: 25.3 MB (11.64%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -0,0 +1,69 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}      # we use
+        self.end = False
+
+class Trie:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+
+    def add(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char]
+        cur.end = True     
+
+    # def print(self) -> None:
+    #     def dfs(cur, prefix):
+    #         if cur.end:
+    #             print(prefix)
+    #         for char, node in cur.children.items():
+    #             dfs(node, prefix + char)
+
+    #     dfs(self.root, "")
+    #     return
+        
+    def getTop3Prefix(self, search_word):
+        cur = self.root
+        res = []
+        prefix = ""
+        for i, char in enumerate(search_word):
+            prefix += char
+            if char not in cur.children:
+                break
+            else:
+                res.append(self.getTop3Char(cur.children[char], prefix))
+            cur = cur.children[char]
+
+        while len(res) < len(search_word):
+            res.append([])
+        return res
+
+
+    def getTop3Char(self, cur, prefix):
+        top3 = []
+
+        def dfs(node, char, prefix):
+            if len(top3) == 3:
+                return
+
+            if node.end:
+                top3.append(prefix)
+
+            for letter in string.ascii_lowercase:
+                if letter in node.children:
+                    dfs(node.children[letter], letter, prefix + letter)
+
+        dfs(cur, prefix[-1], prefix)
+        return top3
+
+
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        trie = Trie()
+        for product in products:
+            trie.add(product)
+        # trie.print()
+        return trie.getTop3Prefix(searchWord)
