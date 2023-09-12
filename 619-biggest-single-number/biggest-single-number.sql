ec2467634971409ec2967913357bdb4cc03ae5a4
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-12 06:13:54 -0400

    Time: 900 ms (66.59%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,26 +1,13 @@
 # Write your MySQL query statement below
 
-# SELECT MAX(num) as num
-# FROM (
-#     SELECT num
-#     FROM mynumbers
-#     GROUP BY num
-#         HAVING COUNT(num) = 1
-# ) mynum;
+# group by numbers, have one with single instance, get largest
 
-WITH cte AS
-(
+SELECT MAX(num) as num
+FROM (
     SELECT num
     FROM mynumbers
     GROUP BY num
         HAVING COUNT(num) = 1
-)
+) cte
 
-# SELECT MAX(num) as num FROM cte;
-SELECT 
-    CASE 
-        WHEN COUNT(num) > 0
-            THEN MAX(num)
-            ELSE NULL
-    END as num
-FROM cte;
+
