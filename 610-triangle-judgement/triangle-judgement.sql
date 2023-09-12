bf1d2af0c8a3242fe2a9dca11aa9410f7a841c8b
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-12 06:08:29 -0400

    Time: 547 ms (72.15%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,19 +1,12 @@
 # Write your MySQL query statement below
 
-# sum of two sides should be greated than the third side
-# a + b > c
-# b + c > a
-# c + a > b
-# if else -> CASE WHEN condition THEN a ELSE b in SQL
-
-# SELECT 
-#     *, 
-#     CASE 
-#         WHEN x+y>z AND y+z>x AND z+x>y THEN 'Yes' 
-#         ELSE 'No' 
-#     END as triangle
-# FROM triangle
-
-SELECT *,
-    IF (x+y>z AND y+z>x AND z+x>y, 'Yes', 'No') as triangle
-FROM Triangle;
+SELECT 
+    *, 
+    CASE
+        WHEN x < y + z
+            AND y < x + z
+            AND z < x + y
+                THEN 'Yes'
+        ELSE 'No'
+    END as triangle
+FROM triangle t
