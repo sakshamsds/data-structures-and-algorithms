5ff79631d17d14f2c1d29228bac581bf87d34429
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-14 21:31:25 -0400

    Time: 86 ms (83.22%) | Memory: 16.4 MB (98.61%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,14 +1,10 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        idx_map = collections.defaultdict(list)
+        count = collections.Counter(s)
         
         for i, c in enumerate(s):
-            idx_map[c].append(i)
-                
-        # print(idx_map)    # ordered
-        for k, v in idx_map.items():
-            if len(v) == 1:
-                return v[0]
+            if count[c] == 1:
+                return i
         
         return -1
         
