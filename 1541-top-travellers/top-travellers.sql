1a45404e0687561bcfd5cc1044d3a42552ef008a
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-15 02:09:40 -0400

    Time: 1563 ms (94.92%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -2,13 +2,9 @@
 
 SELECT 
     u.name,
-    CASE
-        WHEN SUM(r.distance) IS NOT NULL
-            THEN SUM(r.distance)
-        ELSE 0
-    END as travelled_distance
+    IFNULL(SUM(r.distance), 0) as travelled_distance
 FROM users u
 LEFT JOIN rides r
     ON u.id = r.user_id
-GROUP BY u.id, u.name
+GROUP BY u.id
 ORDER BY travelled_distance DESC, u.name
