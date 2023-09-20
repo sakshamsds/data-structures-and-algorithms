d2196a27513a980d4041c5856e5c196bf4adaeb8
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-20 18:35:03 -0400

    Time: 39 ms (67.89%) | Memory: 16.2 MB (69.22%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,15 +1,24 @@
+'''
+0 1
+0 1 
+0 1 1 
+0 1 2 1 
+0 1 3 3 1 
+0 1 4 6 4 1 
+
+
+'''
+
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        
-        dp = [0, 1]
-
         res = []
-        for i in range(1, numRows + 1):
-            new_dp = [0] * (i + 1)
-            for j in range(1, i + 1):
-                new_dp[j] = dp[j - 1] + dp[j]
-            res.append(new_dp[1:])
-            dp = new_dp
-            dp.append(0)
-
+        dp = [0, 1]
+        for r in range(numRows):
+            next_dp = [0] * (r + 1)
+            for i in range(1, r + 1):
+                next_dp[i] = dp[i] + dp[i - 1]
+            next_dp.append(1)
+            res.append(next_dp[1:])
+            dp = next_dp
+            
         return res
