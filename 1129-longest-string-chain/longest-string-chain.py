113d576a665f0a8db9a187622625fb6553243e3b
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-09-23 14:53:29 -0400

    Time: 130 ms (69.16%) | Memory: 20.4 MB (5.78%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.py
+++ 1129-longest-string-chain/longest-string-chain.py
@@ -2,30 +2,29 @@ class Solution:
     def longestStrChain(self, words: List[str]) -> int:
         # use hint, memoization
         # formula, len = max(1, len(pre) + 1)
-        words.sort(key=lambda w: len(w), reverse=True)
+
+        # l = avg len word
+        # TC: O(n * l * l, O(n*logn))
+        # SC: O(n)
+
+        words.sort(key=lambda w: len(w), reverse=True)  # optimization
         words_set = set(words)
-        cache = dict()
+        cache = dict()  # longest chain for every word
 
-        def dfs(word):
-            if word not in words_set:
-                return 0
-
+        def dfs(word):      # will find chain length n times
             if word in cache:
                 return cache[word]
-
-            chain_length = 1
-            for i in range(len(word)):
-                # remove ith char
-                predecessor = word[:i] + word[i + 1:]
-                chain_length = max(chain_length, dfs(predecessor) + 1)
+            chain_size = 1
+            for i in range(len(word)):      # runs size of word times, l
+                pred = word[:i] + word[i + 1:]      # l 
+                if pred in words_set:       # if pred exists
+                    chain_size = max(chain_size, dfs(pred) + 1)
                 # print(word, predecessor)
+            cache[word] = chain_size       # no pred => chain_size = 1
+            return chain_size
 
-            cache[word] = chain_length
-            return chain_length
-
-        max_length = 1
         for word in words:
-            max_length = max(max_length, dfs(word))
+            dfs(word)
 
         # print(cache)
-        return max_length
+        return max(cache.values())
