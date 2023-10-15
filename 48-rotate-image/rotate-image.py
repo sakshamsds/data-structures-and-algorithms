34dc57249b11787ae6a9c2fadc7c6e253522ee80
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-10-14 21:05:15 -0400

    Time: 40 ms (73.51%) | Memory: 16.3 MB (33.82%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,39 +3,14 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
-        # Time Complexity = O(n ^ 2)
-        # one pass
-        n = len(m)
-        for r in range(n//2 + n%2):
-            for c in range(n//2):
-                tmp = m[r][c]
-                m[r][c] = m[n - c - 1][r]
-                m[n - c - 1][r] = m[n - r - 1][n - c - 1]
-                m[n - r - 1][n - c - 1] = m[c][n - r - 1]
-                m[c][n - r - 1] = tmp
-
-        # two pass
-        # n = len(m)
-        # # self.print_mat(m)
-
-        # # swap along main diagonal
-        # for r in range(n):
-        #     for c in range(n - r):
-        #         m[r][c], m[n-c-1][n-r-1] = m[n-c-1][n-r-1], m[r][c]
-        # # self.print_mat(m)
-
-        # # swap along horizontal axis
-        # for r in range(n//2):
-        #     for c in range(n):
-        #         m[r][c], m[n-r-1][c] = m[n-r-1][c], m[r][c]
-
-        # # self.print_mat(m)
-
-
         
-    # def print_mat(self, matrix: List[List[int]]) -> None:
-    #     print()
-    #     for r in matrix:
-    #         print(r)
+        n = len(m)
+        for i in range(n//2):
+            for j in range(i, n - i - 1):
+                temp = m[n - j - 1][i]      # save bottom left
+                m[n - j - 1][i] = m[n - i - 1][n - j - 1]       # update bottom left
+                m[n - i - 1][n - j - 1] = m[j][n - i - 1]       # update bottom right
+                m[j][n - i - 1] = m[i][j]   # update top right
+                m[i][j] = temp      # update top left
+                
         
