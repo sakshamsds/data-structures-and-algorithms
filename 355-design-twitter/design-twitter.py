70a6d8921aa4085fcb04dfb65197c3205fa3b7a9
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-10-25 02:52:38 -0400

    Time: 35 ms (76.69%) | Memory: 16.3 MB (76.25%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -11,7 +11,7 @@ class Twitter:
 
     def __init__(self):
         self.follows = defaultdict(set) # userId - follows mapping
-        self.followedBy = defaultdict(set) # userId - followed_by mapping
+        # self.followedBy = defaultdict(set) # userId - followed_by mapping
         # self.tweets = defaultdict(list)  # userId - tweets
         self.tweets = []
 
@@ -32,12 +32,12 @@ class Twitter:
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.follows[followerId].add(followeeId)
-        self.followedBy[followeeId].add(followerId)
+        # self.followedBy[followeeId].add(followerId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         # discard doesn't throw error if item not found
         self.follows[followerId].discard(followeeId)
-        self.followedBy[followeeId].discard(followerId)
+        # self.followedBy[followeeId].discard(followerId)
 
 
 # Your Twitter object will be instantiated and called as such:
