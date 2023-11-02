6f955d4482830a82e8d19f788733eca68e123ff0
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-01 21:29:49 -0400

    Time: 50 ms (52.94%) | Memory: 17.5 MB (58.02%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
@@ -7,27 +7,25 @@
 class Solution:
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
         # post order traversal
-        self.cnt = 0
+        res = 0
 
         def dfs(node):
-            subtree_sum = node.val
-            subtree_cnt = 1
+            nonlocal res 
+        
+            if node is None:
+                return 0, 0
 
-            if node.left:
-                l_sum, l_cnt = dfs(node.left)
-                subtree_sum += l_sum
-                subtree_cnt += l_cnt
+            l_sum, l_cnt = dfs(node.left)
+            r_sum, r_cnt = dfs(node.right)
 
-            if node.right:
-                r_sum, r_cnt = dfs(node.right)
-                subtree_sum += r_sum
-                subtree_cnt += r_cnt
+            subtree_sum = node.val + l_sum + r_sum
+            subtree_cnt = 1 + l_cnt + r_cnt
 
             avg = subtree_sum // subtree_cnt
             if avg == node.val:
-                self.cnt += 1
+                res += 1
             
             return subtree_sum, subtree_cnt
             
         dfs(root)
-        return self.cnt
+        return res
