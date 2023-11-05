d4350b1783485c74f13f9f0bb9c5613cce6bf097
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-04 23:40:45 -0400

    Time: 505 ms (95.11%) | Memory: 29.9 MB (63.04%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
@@ -1,27 +1,16 @@
-# no duplicates
-
-
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
-        q = collections.deque(arr)
-        winner = q.popleft()
-        new_num = q.popleft()
+        current_winner = arr[0]
+        wins = 0
 
-        num_wins = 0
-        prev_winner = -1
-
-        while num_wins < k:
-            q.append(min(winner, new_num))     # push min to q before updating winner
-            winner = max(winner, new_num)
-            # print(q)
-            # print(winner)
-            if winner == prev_winner:   # greater than all other arra elements
-                num_wins += 1
-                if num_wins == len(arr) - 1:
-                    return winner
+        for i in range(1, len(arr)):
+            if current_winner > arr[i]:
+                wins += 1
             else:
-                num_wins = 1
-            new_num = q.popleft()
-            prev_winner = winner
-
-        return winner
+                current_winner = arr[i]
+                wins = 1
+            
+            if wins == k:
+                return current_winner
+        
+        return current_winner
