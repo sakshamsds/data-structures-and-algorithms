4e38fee5108aaa914ee8541c40fa559f351580e8
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-15 02:48:14 -0500

    Time: 551 ms (74.64%) | Memory: 18.7 MB (32.20%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -2,30 +2,30 @@ class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         rows, cols = len(heights), len(heights[0])
 
-        # heap with tuple (cur_max_diff, r, c)
-        heap = [(0, 0, 0)]
+        pq = [(0, 0, 0)]       # (effort, r, c)
         visited = set()
-        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
 
-        # greedy bfs/dijkstra
-        while heap:
-            cur_max_diff, r, c = heapq.heappop(heap)
-            # print(r, c, cur_max_diff)
-            if (r, c) in visited:               # base case 1
+        while pq:
+            cur_effort, r, c = heapq.heappop(pq)
+
+            if (r, c) in visited:
                 continue
+
             visited.add((r, c))
-            if r == rows - 1 and c == cols - 1:     # base case 2, reached the end
-                return cur_max_diff
 
-            for dr, dc in directions:
+            if (r, c) == (rows - 1, cols - 1):
+                return cur_effort
+
+            for dr, dc in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
                 nr, nc = r + dr, c + dc
-                if (                                # validate neighbor
-                    nr < 0 or nr == rows or
-                    nc < 0 or nc == cols or
-                    (nr, nc) in visited
-                ):
-                    continue
-                new_diff = max(cur_max_diff, abs(heights[nr][nc] - heights[r][c]))
-                heapq.heappush(heap, (new_diff, nr, nc))
 
-        return 0
+                # validate nbr
+                if (nr < 0 or nr >= rows or
+                    nc < 0 or nc >= cols or
+                    (nr, nc) in visited):
+                    continue
+
+                effort = max(cur_effort, abs(heights[nr][nc] - heights[r][c]))
+                heapq.heappush(pq, (effort, nr, nc))
+
+        return -1
