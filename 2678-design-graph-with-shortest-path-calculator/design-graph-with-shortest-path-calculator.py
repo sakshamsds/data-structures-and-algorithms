f3bdc20577900c7bbe86e377d57c1687f6409906
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-15 01:53:26 -0500

    Time: 1424 ms (22.79%) | Memory: 20 MB (6.19%) - LeetSync

--- 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.py
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.py
@@ -1,48 +1,32 @@
-'''
-pass through all the vertices available, dp
-2 -> 5
-    : 2 -> 1 + 1 -> 5
-    : 2 -> 2 + 2 -> 5
-            .
-            .
-            .
-    : 2 -> n + n -> 5
-'''
-
 class Graph:
 
     def __init__(self, n: int, edges: List[List[int]]):
-        # make adj matrix
-        self.adj_matrix = [[float('inf')] * n for _ in range(n)]
+        self.adj_list = collections.defaultdict(list)
         for start, end, cost in edges:
-            self.adj_matrix[start][end] = cost 
-
-        for i in range(n): 
-            self.adj_matrix[i][i] = 0
-
-        # floyd warshall
-        for via in range(n):
-            for i in range(n):
-                for j in range(n):
-                    self.adj_matrix[i][j] = min(self.adj_matrix[i][j], 
-                                                self.adj_matrix[i][via] + 
-                                                self.adj_matrix[via][j])
+            self.adj_list[start].append((end, cost))
+        self.n = n
 
     def addEdge(self, edge: List[int]) -> None:
         start, end, cost = edge
-        n = len(self.adj_matrix)
-
-        # via node becomes start -> end
-        for i in range(n):
-            for j in range(n):
-                self.adj_matrix[i][j] = min(self.adj_matrix[i][j], 
-                                            self.adj_matrix[i][start] + 
-                                            cost + 
-                                            self.adj_matrix[end][j])
+        self.adj_list[start].append((end, cost))
 
     def shortestPath(self, node1: int, node2: int) -> int:
-        cost = self.adj_matrix[node1][node2]
-        return -1 if cost == float('inf') else cost
+        # apply dijkstra
+        distances = [float('inf')] * self.n
+        pq = [(0, node1)]
+        distances[node1] = 0        # distance to sources node = 0
+
+        while pq:
+            dist, node = heapq.heappop(pq)
+            for nbr, edge_weight in self.adj_list[node]:
+                if dist + edge_weight < distances[nbr]:
+                    distances[nbr] = dist + edge_weight
+                    heapq.heappush(pq, (distances[nbr], nbr))
+
+        if distances[node2] == float('inf'):
+            return -1
+        return distances[node2] 
+        
 
 
 # Your Graph object will be instantiated and called as such:
