1e5ebafc296de09842bd6d9581c85bc6d772f52c
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-16 01:43:34 -0500

    Time: 43 ms (64.94%) | Memory: 16.8 MB (8.31%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,28 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
-        cache = {}
-        cache[len(s)] = True
-
-        # @cache
+        @cache
         def dfs(i):
-            # if i == len(s):   # base case
-                # return True
+            if i == len(s):   # base case
+                return True
             # print(i)
-            if i in cache:
-                return cache[i]
 
             # at every idx try all the words to see if you get a match
             for w in wordDict:
                 if i + len(w) <= len(s) and s[i:i+len(w)] == w:
                     if dfs(i + len(w)):
-                        cache[i] = True
                         return True
-
-            cache[i] = False
             return False
 
         return dfs(0)
-
-
         
