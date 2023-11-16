6e1a9d6f06d2f7b8acc35728b78287902a9c383d
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-16 00:11:35 -0500

    Time: 45 ms (38.96%) | Memory: 16.2 MB (86.18%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,14 +1,15 @@
-from typing import List
+'''
+2, 2 -> total = 2^n = 4 - 2 = 2
+n = 3, total = 2^3 = 8 - 3 = 5
+'''
 
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         n = len(nums)
-        
-        # Use a set to store the binary representation directly
-        available = set(nums)
-        
-        max_num = 2 ** n
-        for num in range(max_num):
-            binary_str = bin(num)[2:].zfill(n)  # Convert to binary and ensure leading zeros
-            if binary_str not in available:
-                return binary_str
+        available = set()
+        for binary in nums:
+            available.add(int(binary, 2))
+
+        for num in range(n + 1):    # for n = 2, available should be 0, 1, 2, 3
+            if num not in available:
+                return bin(num)[2:].zfill(n)
