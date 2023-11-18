0f53511e85f6ea86fb7651cbb7cfc7c81f65e61d
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-18 17:59:14 -0500

    Time: 1007 ms (96.68%) | Memory: 31.2 MB (18.67%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
@@ -8,9 +8,9 @@ class Solution:
         for r in range(len(nums)):
             window_sum += nums[r]
             # condition: sum of window + k >= size of window * max_num
-            while window_sum + k < (r - l + 1) * nums[r]:
+            # editorial: we will not shrink the window - we will just try to grow it as large as we can
+            if window_sum + k < (r - l + 1) * nums[r]:
                 window_sum -= nums[l]
                 l += 1
-            res = max(res, (r - l + 1))
 
-        return res
+        return r - l + 1
