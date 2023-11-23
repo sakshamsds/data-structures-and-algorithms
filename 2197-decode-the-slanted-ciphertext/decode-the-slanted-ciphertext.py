bc9aa04096220e33e56bc3f32f94856f55389b8e
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-23 01:53:57 -0500

    Time: 368 ms (46.97%) | Memory: 35.1 MB (19.70%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.py
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.py
@@ -1,24 +1,10 @@
+# lee215 solution
+
 class Solution:
     def decodeCiphertext(self, encodedText: str, rows: int) -> str:
         cols = len(encodedText)//rows
-        # print('numCols', cols)
-
-        mat = [encodedText[cols*r:cols*r + cols] for r in range(rows)]
-        # print(mat)
-
         res = []
         for i in range(cols):
-            for j in range(rows):
-                r, c = j, j + i
-                # print(r, c)
-                res.append(mat[r][c])
-                if c == cols - 1:
-                    break
-
-        i = 0
-        for i in range(len(res) - 1, -1, -1):
-            if res[i] != ' ':
-                break
-
-        # print(i)
-        return "".join(res[:i + 1])
+            for j in range(i, len(encodedText), cols + 1):
+                res.append(encodedText[j])
+        return "".join(res).rstrip()
