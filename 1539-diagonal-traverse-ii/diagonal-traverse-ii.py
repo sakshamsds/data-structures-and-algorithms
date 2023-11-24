dc414fbf6e72c9c97e949db01f6b2a77951c6c98
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-23 22:22:37 -0500

    Time: 765 ms (92.02%) | Memory: 41.3 MB (41.11%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -1,12 +1,25 @@
+'''
+    0   1   2   3   4   5
+0   0   1   2   3   4   5
+1   1   2   3   4   5   6
+2   2   3   4   5   6   7
+3   3   4   5   6   7   8
+4   4   5   6   7   8   9
+5   5   6   7   8   9   10
+'''
+
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
         diagDict = collections.defaultdict(list)
-        for i, row in enumerate(nums):
-            for j in range(len(row)):
-                diagDict[i + j].append(row[j])
+        for r in range(len(nums) - 1, -1, -1):
+            for c in range(len(nums[r])):
+                diagDict[r + c].append(nums[r][c])
 
+        print(diagDict.keys())
         res = []
-        for k, v in diagDict.items():
-            res.extend(v[::-1])
+        key = 0
+        while key in diagDict:
+            res.extend(diagDict[key])
+            key += 1
 
         return res
