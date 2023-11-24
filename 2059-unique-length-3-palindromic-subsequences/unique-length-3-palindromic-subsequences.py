6b6b9529ecc6e372d4b440c75de5a0fec5414afb
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-24 00:29:03 -0500

    Time: 304 ms (56.32%) | Memory: 17.3 MB (43.38%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,10 +1,24 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        letters = set(s)
+        # precompute the indices
+        startIdxs = [-1] * 26
+        endIdxs = [-1] * 26
+
+        for i, char in enumerate(s):
+            idx = ord('a') - ord(char)
+            if startIdxs[idx] == -1:
+                startIdxs[idx] = i
+
+        for i in range(len(s) - 1, -1, -1):
+            idx = ord('a') - ord(s[i])
+            if endIdxs[idx] == -1:
+                endIdxs[idx] = i
 
         count = 0
-        for letter in letters:
-            l, r = s.index(letter), s.rindex(letter)
+        for i in range(26):
+            l, r = startIdxs[i], endIdxs[i]
+            if l == -1:
+                continue
             count += len(set(s[l + 1:r]))
 
         return count
