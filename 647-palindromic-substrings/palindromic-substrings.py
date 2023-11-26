591197374c3a5d397d74c61b0dbffc33e0ffa0fa
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-26 14:46:18 -0500

    Time: 96 ms (77.77%) | Memory: 16.3 MB (59.01%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -7,8 +7,8 @@ class Solution:
         for i in range(len(s)):
             for l, r in [(i, i), (i, i + 1)]:
                 while l >= 0 and r < len(s) and s[l] == s[r]:
-                    res += 1
                     l -= 1
                     r += 1
+                res += (r - l) // 2
 
         return res
