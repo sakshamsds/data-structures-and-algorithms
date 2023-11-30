247b1479f88b83e90425634d4646215785a0446c
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-11-29 21:17:35 -0500

    Time: 50 ms (67.14%) | Memory: 16.3 MB (23.56%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -7,23 +7,14 @@ class Solution:
             'L': 50,
             'C': 100,
             'D': 500,
-            'M': 1000,
-            'IV': 4,
-            'IX': 9,
-            'XL': 40,
-            'XC': 90,
-            'CD': 400,
-            'CM': 900
+            'M': 1000
         }
         
         res = 0
-        i = 0
-        while i < len(s): 
-            if (s[i] == 'I' or s[i] == 'X' or s[i] == 'C') and i < len(s) - 1 and s[i:i+2] in valueDict:
-                res += valueDict[s[i:i+2]]
-                i += 1
+        for i in range(len(s)):
+            if i < len(s) - 1 and valueDict[s[i]] < valueDict[s[i + 1]]:
+                res -= valueDict[s[i]]
             else:
                 res += valueDict[s[i]]
-            i += 1
-        
+
         return res
