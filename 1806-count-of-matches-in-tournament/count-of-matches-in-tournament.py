0f3f2c340c72cba611aca4a1478e17514391ea17
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-12-04 19:41:34 -0500

    Time: 37 ms (59.83%) | Memory: 16.4 MB (5.91%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -1,7 +1,11 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        res = 0
-        while n > 1:
-            res += n // 2
-            n = ceil(n / 2)
-        return res
+        # n teams, 1 winner, n - 1 losers
+        # each match eliminates 1 team
+        return n - 1
+
+        # res = 0
+        # while n > 1:
+            # res += n // 2
+            # n = ceil(n / 2)
+        # return res
