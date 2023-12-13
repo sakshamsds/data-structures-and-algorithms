6cc2443108e4f3aa50854c91ff9c68952b35191a
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-12-13 03:29:16 -0500

    Time: 152 ms (69.11%) | Memory: 16.8 MB (33.33%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -1,21 +1,18 @@
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
-        # brute force
         m, n = len(mat), len(mat[0])
-        res = 0
+        row_sum = [0] * m
+        col_sum = [0] * n
         for r in range(m):
             for c in range(n):
                 if mat[r][c] == 1:
-                    extra_1 = False
-                    for c0 in range(n):
-                        if c0 != c and mat[r][c0] == 1:
-                            extra_1 = True
-                            break
-                    for r0 in range(m):
-                        if r0 != r and mat[r0][c] == 1:
-                            extra_1 = True
-                            break
-                    if not extra_1:
-                        res += 1
+                    row_sum[r] += 1
+                    col_sum[c] += 1
 
-        return res
+        res = 0
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 1 and row_sum[r] == 1 and col_sum[c] == 1:
+                    res += 1
+
+        return res  
