12370f5e7a431b5cdddc3af4e5833bd039f43c3d
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-12-18 19:42:37 -0500

    Time: 490 ms (60.54%) | Memory: 17 MB (59.70%) - LeetSync

---
+++ 661-image-smoother/image-smoother.py
@@ -0,0 +1,17 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(img), len(img[0])
+        points = [(0, 0), (0, 1), (0, -1), (1, 1), (1, 0), (1, -1), (-1, 1), (-1, 0), (-1, -1)]
+        res = [[0] * cols for _ in range(rows)]
+
+        for r in range(rows):
+            for c in range(cols):
+                agg = 0
+                cnt = 0
+                for dr, dc in points:
+                    nr, nc = r + dr, c + dc
+                    if (0 <= nr < rows and 0 <= nc < cols):
+                        agg += img[nr][nc]
+                        cnt += 1
+                res[r][c] = floor(agg/cnt)
+        return res
