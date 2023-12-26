095e16166ca7edfbbeda3f07b9aa132c6277bbeb
Sakshamdeep Singh <saksham.sds@gmail.com>
2023-12-26 16:22:49 -0500

    Time: 128 ms (89.96%) | Memory: 19.3 MB (40.36%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
@@ -1,13 +1,12 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        
         # 1 2 3 ... k or target whichever is the min
         # store n, target
         MOD = 1e9 + 7
         cache = {}
 
         def dfs(n, cur_target):
-            if cur_target <= 0 or cur_target > (n * k): # target out of range
+            if cur_target == 0 or cur_target > (n * k): # target out of range
                 return 0
 
             if n == 1:  # keep rolling until n == 1
@@ -17,8 +16,7 @@ class Solution:
                 return cache[(n, cur_target)]
 
             ways = 0
-            for i in range(1, k + 1):  # roll the dice again
-            # for i in range(1, min(cur_target, k) + 1):  # roll the dice again
+            for i in range(1, min(cur_target, k) + 1):  # roll the dice again
                 ways += dfs(n - 1, cur_target - i)
             cache[(n, cur_target)] = ways
             return ways
