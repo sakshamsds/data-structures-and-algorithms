be51425b5ff8497eca69a324ef46501e9f24187a
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-01 18:46:20 -0500

    Time: 114 ms (6.10%) | Memory: 17.2 MB (26.60%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,28 +1,25 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        array_length = 0
-        l = 0
-        freq = 1
+        l, freq = 0, 1
         prev = chars[0]
         for r in range(1, len(chars)):
             if chars[r] == prev:
                 freq += 1
             else:
-                chars[l] = prev     # store char in l 
-                l += 1
-                if freq > 1:
-                    for digit in str(freq):  # store freq in the next few elements
-                        chars[l] = digit
-                        l += 1
+                l = self.populateChars(chars, freq, l, prev)
                 freq = 1            # new freq is 1
             prev = chars[r]
         
         # for last char and freq
-        chars[l] = prev     # store char in l 
-        l += 1
+        l = self.populateChars(chars, freq, l, prev)
+        return l
+
+    def populateChars(self, chars, freq, idx, prev):
+        chars[idx] = prev     # store char in l 
+        idx += 1
         if freq > 1:
             for digit in str(freq):     # store freq in the next few elements
-                chars[l] = digit
-                l += 1
-        return l
+                chars[idx] = digit
+                idx += 1
+        return idx
 
