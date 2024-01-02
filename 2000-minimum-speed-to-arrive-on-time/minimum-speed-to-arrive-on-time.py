c0e25e8ab0c957d33d3c5fc4187b0a27d4ca15b7
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-01 21:38:31 -0500

    Time: 4703 ms (5.01%) | Memory: 31.3 MB (20.35%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+
+        def getTime(speed):
+            arrival_time = 0
+            for i in range(len(dist) - 1):
+                arrival_time += ceil(dist[i]/speed)
+            return arrival_time + dist[-1]/speed
+
+        l, r = 1, max(dist) * 100
+
+        while l < r:
+            mid = l + (r - l) // 2
+            arr_time = getTime(mid)
+            if arr_time > hour:
+                l = mid + 1
+            else:
+                r = mid
+
+        return l if getTime(l) <= hour else -1
