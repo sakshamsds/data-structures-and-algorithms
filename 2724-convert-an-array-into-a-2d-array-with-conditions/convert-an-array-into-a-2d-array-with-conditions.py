ad2dad05d24becf6e174be2e5be8a021fe111c44
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-01 20:11:45 -0500

    Time: 89 ms (5.01%) | Memory: 17.3 MB (12.28%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,13 +1,13 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        freqDict = collections.Counter(nums)
+        freqDict = defaultdict(int)
         arr2D = []
-        for num, freq in freqDict.items():
-            for i in range(freq):
-                if i < len(arr2D):      # row already exits
-                    arr2D[i].append(num)
-                else:
-                    arr2D.append([num])     # add new row
+        for num in nums:
+            freq = freqDict[num]
+            if freq == len(arr2D):
+                arr2D.append([])
+            arr2D[freq].append(num)
+            freqDict[num] += 1
         return arr2D
 
         
