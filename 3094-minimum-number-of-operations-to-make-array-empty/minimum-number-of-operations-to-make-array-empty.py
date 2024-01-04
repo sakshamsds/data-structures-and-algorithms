0418beb533528d435621f162dfedce3e711a8362
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-04 01:03:51 -0500

    Time: 528 ms (99.09%) | Memory: 32.3 MB (6.06%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -1,30 +1,9 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        freqDict = collections.Counter(nums)
-        total_ops = 0
-        # print(freqDict)
-        for _, freq in freqDict.items():
-            ops = self.getOps(freq)
-            if ops == -1:
-                return -1
-            else:
-                total_ops += ops
-        return total_ops
-
-    # 10
-    # 2, 2, 2, 2, 2
-    # 3, 3, 2, 2
-
-    def getOps(self, freq):
+        freq_dict = collections.Counter(nums)
         ops = 0
-        while freq > 0:
+        for _, freq in freq_dict.items():
             if freq == 1:
                 return -1
-            if freq % 3 == 0:
-                ops += freq // 3
-                return ops
-            freq -= 2           # keep subtracting by 2, reach either 0 or 1
-            ops += 1
+            ops += ceil(freq / 3)
         return ops
-
-    # 2 2 2 2 2 2 2 2 2 2 | 2 2
