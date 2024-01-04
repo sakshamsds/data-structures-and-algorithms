21e72e11760fd322f2dd4a962b1f7b5e1e183fd1
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-04 05:29:16 -0500

    Time: 469 ms (77.59%) | Memory: 17.6 MB (75.25%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,30 +1,33 @@
 '''
-lSum, rSum, idx
-lSum, rSum = total - left, idx
-lSum, idx
+        0   1   2   3   4   5   6   7   8   9   10  11
+0       T   F   F   F   F   F   F   F   F   F   F   F
+1       T   T   F   F   F   F   F   F   F   F   F   F
+5       T   T   F   F   F   T   T   F   F   F   F   F
+11      T   T   F   F   F   T   T   F   F   F   F   T
+5       T   T   F   F   F   T   T   F   F   F   F   T
+
+        0   1   2   3   4
+0       T   F   F   F   F
+1       T   T   F   F   F
+2       T   T   T   T   F
+5       T   T   T   T   F
 '''
+
+
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total_sum = sum(nums)
-        if total_sum % 2 == 1:
+        # can we make target given the nums, variant of coin change, 0/1 knapsack
+        total = sum(nums)
+        if total % 2 == 1:
             return False
+        target = total // 2
 
-        target_sum = total_sum // 2
-        cache = {}
+        dp = [True] + [False] * target
+        for num in nums:
+            for t in range(target, num - 1, -1):
+                dp[t] = dp[t] or dp[t - num]
+                if dp[target]:
+                    return True
 
-        def dfs(idx, l_sum):
-            if idx == len(nums):
-                return l_sum == target_sum
-
-            if l_sum > target_sum:        # stop if lSum goes over target
-                return False
-
-            if (idx, l_sum) in cache:
-                return cache[(idx, l_sum)]
-                
-            cache[(idx, l_sum)] = dfs(idx + 1, l_sum) or \
-                                  dfs(idx + 1, l_sum + nums[idx])
-            return cache[(idx, l_sum)]
-
-        return dfs(0, 0)
+        return dp[target]
