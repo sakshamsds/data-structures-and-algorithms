646a10da476521bb3e1e633a58f275d6a7618a91
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-13 00:00:25 -0500

    Time: 153 ms (63.58%) | Memory: 17.9 MB (21.41%) - LeetSync

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -0,0 +1,26 @@
+'''
+bab - a1 b2
+aba - a2 b1
+
+leetcode -    c1 d1 e3    l1 o1       t1
+practice - a1 c2    e1 i1       p1 r1 t1 
+
+(total chars - common char) / 2
+1 + 1 + 1 + 2 + 1 + 1 + 1 + 1
+5
+'''
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        s_freq = [0] * 26
+        t_freq = [0] * 26
+
+        for c1, c2 in zip(s, t):
+            s_freq[ord(c1) - ord('a')] += 1
+            t_freq[ord(c2) - ord('a')] += 1
+
+        diff = 0
+        for i in range(26):
+            diff += abs(t_freq[i] - s_freq[i])
+
+        return diff // 2
