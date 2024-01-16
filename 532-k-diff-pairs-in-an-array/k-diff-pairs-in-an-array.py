421f54cc82b68af7e95a3789d9a52a21600044cd
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-16 10:31:22 -0500

    Time: 68 ms (87.51%) | Memory: 19.1 MB (30.67%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -2,7 +2,7 @@ class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
         res = 0
 
-        if k == 0:  # check repeated numbers
+        if k == 0:  # check repeated numbers, edge case
             counter = Counter(nums)
             for k, v in counter.items():
                 if v > 1:
@@ -10,14 +10,11 @@ class Solution:
             return res
 
         vals = set(nums)
-        pairs = []
         while vals:
             num = vals.pop()
             left, right = num - k, num + k
             if left in vals:
-                pairs.append([num, left])
                 res += 1
             if right in vals:
-                pairs.append([num, right])
                 res += 1 
         return res
