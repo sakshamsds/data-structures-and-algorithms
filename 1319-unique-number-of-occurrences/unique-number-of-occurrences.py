d9959dfbc457249dfeb0c1055957c695cdd7838e
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-17 06:48:30 -0500

    Time: 37 ms (88.35%) | Memory: 17.4 MB (25.02%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,17 +1,11 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        freqs = [0] * 2001
-        # -1000 -> 0
-        # 0 -> 1000
-        # 1000 -> 2001
-
-        for num in arr:
-            freqs[num + 1000] += 1
-
-        unique = set()
-        for freq in freqs:
-            if freq != 0 and freq in unique:
+        freqs = Counter(arr)    
+        
+        occurences = set()
+        for freq in freqs.values():
+            if freq in occurences:
                 return False
-            unique.add(freq)
+            occurences.add(freq)
 
         return True
