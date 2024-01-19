55e4701206d879028f1ba6664f1868c42eed8a64
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-19 09:41:14 -0500

    Time: 7 ms (54.05%) | Memory: 45.6 MB (14.10%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -5,15 +5,12 @@ class Solution {
             matrix[r][0] += Math.min(matrix[r - 1][0], matrix[r - 1][1]);
             matrix[r][n - 1] += Math.min(matrix[r - 1][n - 1], matrix[r - 1][n - 2]);
             for (int c = 1; c < n - 1; c++) {
-                matrix[r][c] += Math.min(matrix[r - 1][c - 1], Math.min(
-                                        matrix[r - 1][c],
-                                        matrix[r - 1][c + 1]));
+                matrix[r][c] += Math.min(matrix[r - 1][c - 1], 
+                                        Math.min(matrix[r - 1][c],
+                                                matrix[r - 1][c + 1]));
             }
         }
-        int minSum = matrix[n - 1][0];
-        for (int c = 1; c < n; c++) {
-            minSum = Math.min(minSum, matrix[n - 1][c]);
-        }
-        return minSum;
+
+        return Arrays.stream(matrix[n - 1]).min().getAsInt();
     }
 }
