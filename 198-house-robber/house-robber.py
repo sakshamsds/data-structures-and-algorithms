0b75d1000b9a54e01695510b068682429a1e68f3
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-21 12:03:48 -0500

    Time: 40 ms (46.58%) | Memory: 16.4 MB (60.22%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,10 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # we didn't rob the previous house so we rob the current house
-        # we robbed the previous hosue so we cannot rob the current house
-        no_rob, rob_cur = 0, 0
-        
+        prev, curr = 0, 0
         for num in nums:
-            no_rob, rob_cur = max(no_rob, rob_cur), no_rob + num
-            
-        return max(no_rob, rob_cur)
+            curr, prev = prev + num, max(prev, curr)
+        return max(prev, curr)
