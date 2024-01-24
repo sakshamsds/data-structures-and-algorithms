7ba6fe6167a2f4908ca10171e400bedb71f2fb71
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-24 13:55:29 -0500

    Time: 42 ms (32.80%) | Memory: 16.6 MB (57.35%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,7 +1,6 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        opening_brackets = {'(', '[', '{'}
-        matching = {
+        matches = {
             ')': '(',
             ']': '[',
             '}': '{'
@@ -9,15 +8,12 @@ class Solution:
         stack = []
 
         for bracket in s:
-            if bracket in opening_brackets:     # opening bracket
+            if bracket not in matches:     # opening bracket
                 stack.append(bracket)
             else:                               # closing bracket
-                if len(stack) == 0:             # '}[]'
-                    return False
+                if stack and stack[-1] == matches[bracket]:
+                    stack.pop()
                 else:
-                    if stack[-1] == matching[bracket]:    # matches
-                        stack.pop()
-                    else:
-                        return False
+                    return False
 
-        return len(stack) == 0
+        return not stack
