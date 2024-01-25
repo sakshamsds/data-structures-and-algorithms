3de4586dd31ece85018f38975f051f8172b65bd9
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-25 11:11:33 -0500

    Time: 1639 ms (66.13%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,10 +1,27 @@
 # Write your MySQL query statement below
 
-
-SELECT student_id, student_name, subject_name, COUNT(e.student_id) as attended_exams
-FROM students st
-CROSS JOIN subjects su
-LEFT JOIN examinations e
+WITH cte AS (
+    SELECT *
+    FROM students
+    CROSS JOIN subjects
+),
+cte2 as (
+    SELECT 
+        student_id,
+        subject_name,
+        COUNT(subject_name) AS attended_exams
+    FROM examinations
+    GROUP BY student_id, subject_name
+)
+SELECT 
+    cte.student_id,
+    cte.student_name,
+    cte.subject_name,
+    CASE WHEN attended_exams IS NULL
+            THEN 0
+         ELSE attended_exams
+    END as attended_exams
+FROM cte
+LEFT JOIN cte2
     USING (student_id, subject_name)
-GROUP BY student_id, subject_name
 ORDER BY student_id, subject_name;
