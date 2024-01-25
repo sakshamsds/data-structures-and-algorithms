c6f9a7870548fe35ecb02e4799e3f09ef6e6ce69
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-24 19:54:00 -0500

    Time: 536 ms (69.09%) | Memory: 16.6 MB (95.88%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,22 +1,28 @@
 '''
-    0   a   b   c   d   e
-0   0   0   0   0   0   0
-a   0   1   1   1   1   1
-c   0   1   1   2   2   2
-e   0   1   1   2   2   3
+    -   a   b   c   d   e   e
+-   0   0   0   0   0   0   0
+a   0   1   1   1   1   1   1
+c   0   1   1   2   2   2   2
+e   0   1   1   2   2   3   3
+
+abcd[e]
+ac[e]
+
+abcde
+ac[e]
+
+abcd[e]
+ace
 '''
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         m, n = len(text1), len(text2)
-        prev_dp = [0] * (n + 1)     # row 0
-
-        for i in range(1, m + 1):
-            next_dp = [0] * (n + 1)
+        dp = [0] * (n + 1)
+        for i in range(m):
+            new_dp = [0] * (n + 1)
             for j in range(1, n + 1):
-                next_dp[j] = max(prev_dp[j], next_dp[j - 1])
-                if text1[i - 1] == text2[j - 1]:
-                    next_dp[j] = max(next_dp[j], prev_dp[j - 1] + 1)
-            prev_dp = next_dp
-
-        return next_dp[-1]
+                incre = 1 if text1[i] == text2[j - 1] else 0
+                new_dp[j] = max(new_dp[j - 1], dp[j], dp[j - 1] + incre)
+            dp = new_dp
+        return new_dp[-1]
