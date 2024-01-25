2a82df1dbad395d7107cf893bfec928180e4168c
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-25 12:06:11 -0500

    Time: 630 ms (54.43%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,15 +1,8 @@
 # Write your MySQL query statement below
 
-SELECT name
-FROM (
-    SELECT
-        e2.id,
-        e2.name,
-        COUNT(*) as c
-    FROM employee e1
-    LEFT JOIN employee e2
-        ON e1.managerId = e2.id
-    GROUP BY e2.id
-) AS subtable
-WHERE c > 4
-    AND id IS NOT NULL;
+SELECT name FROM employee
+WHERE id IN (
+    SELECT managerId FROM employee
+    GROUP BY managerId
+        HAVING COUNT(managerId) >= 5
+);
