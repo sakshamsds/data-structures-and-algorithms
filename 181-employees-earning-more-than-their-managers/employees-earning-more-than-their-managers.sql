d753d83f9fa347b75e5393fd55b3189df6b25d0b
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-25 16:07:49 -0500

    Time: 591 ms (94.54%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,21 +1,7 @@
 # Write your MySQL query statement below
 
-# SELECT 
-#     e.name as Employee
-# FROM 
-#     employee as e,
-#     employee as m
-# WHERE 
-#     e.managerid = m.id
-#     AND e.salary > m.salary
-
-SELECT e1.name as Employee
-FROM Employee as e1            # need to give alias for self join
-INNER JOIN Employee as e2
-    ON e1.managerId = e2.id
-WHERE e1.salary > e2.salary;
-
-
-# INNER JOIN, records in both the tables
-# LEFT JOIN, records in left + intersection
-# we don't neet left join for above, means manager is not present
+SELECT e.name AS Employee
+FROM employee e
+INNER JOIN employee m
+    ON e.managerId = m.id
+WHERE e.salary > m.salary;
