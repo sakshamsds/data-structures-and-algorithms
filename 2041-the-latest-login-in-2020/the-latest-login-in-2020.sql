c0755ae105ba4edac776eb6ea07d755f3bbd4c00
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-27 00:23:55 -0500

    Time: 1135 ms (63.72%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,11 +1,24 @@
 # Write your MySQL query statement below
 
 SELECT 
-    user_id,
-    MAX(CASE WHEN YEAR(time_stamp) = 2020
-                THEN time_stamp
-             ELSE -1 
-        END) AS last_stamp
+    DISTINCT user_id,
+    FIRST_VALUE(time_stamp) OVER(PARTITION BY user_id ORDER BY time_stamp DESC)  AS last_stamp
 FROM logins
-GROUP BY user_id
-    HAVING last_stamp > 0;
+WHERE YEAR(time_stamp) = 2020;
+
+-- SELECT
+--     user_id,
+--     MAX(time_stamp) as last_stamp
+-- FROM logins
+-- WHERE YEAR(time_stamp) = 2020
+-- GROUP BY user_id;
+
+-- SELECT 
+--     user_id,
+--     MAX(CASE WHEN YEAR(time_stamp) = 2020
+--                 THEN time_stamp
+--              ELSE -1 
+--         END) AS last_stamp
+-- FROM logins
+-- GROUP BY user_id
+--     HAVING last_stamp > 0;
