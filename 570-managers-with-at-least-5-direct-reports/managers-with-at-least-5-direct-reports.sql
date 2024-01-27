8d0d1ca9b82407cac47b2fd47b7b1751f22a606c
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-27 13:03:16 -0500

    Time: 555 ms (81.44%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,12 +1,20 @@
 # Write your MySQL query statement below
 
-WITH cte AS (
-    SELECT managerId
-    FROM employee
-    GROUP BY managerId
-        HAVING COUNT(*) > 4
-)
-SELECT name
-FROM cte
-INNER JOIN employee e
-    ON cte.managerId = e.id;
+SELECT 
+    m.name
+FROM employee e
+INNER JOIN employee m
+    ON e.managerId = m.id
+GROUP BY m.id
+    HAVING COUNT(m.id) > 4;
+
+-- WITH cte AS (
+--     SELECT managerId
+--     FROM employee
+--     GROUP BY managerId
+--         HAVING COUNT(*) > 4
+-- )
+-- SELECT name
+-- FROM cte
+-- INNER JOIN employee e
+--     ON cte.managerId = e.id;
