d27d12ae5ce59dafb04559c97ec38a6be0e23c6c
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-28 14:20:52 -0500

    Time: 990 ms (93.07%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -2,7 +2,6 @@
 
 SELECT 
     user_id,
-    -- CONCAT(UPPER(LEFT(name, 1)), LOWER(RIGHT(name, LENGTH(name) - 1))) as name
-    CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2, LENGTH(name)))) as name
+    CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) AS name
 FROM users
 ORDER BY user_id;
