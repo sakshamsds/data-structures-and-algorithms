dec3a5946aefa598a0def515c107af9de5309c20
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-28 14:25:19 -0500

    Time: 2568 ms (33.96%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,14 +1,13 @@
 # Write your MySQL query statement below
 
 WITH cte AS (
-    SELECT
+    SELECT 
         person_name,
         SUM(weight) OVER(ORDER BY turn) as total_weight
     FROM queue
 )
 SELECT 
     person_name
-    -- total_weight
 FROM cte
 WHERE total_weight <= 1000
 ORDER BY total_weight DESC
