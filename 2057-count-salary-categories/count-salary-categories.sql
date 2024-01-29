280e01c24674a057fde4a61538105cbf0f1c6eec
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-28 23:18:45 -0500

    Time: 2958 ms (33.95%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,23 +1,19 @@
 # Write your MySQL query statement below
 
-WITH cte1 AS (
-    SELECT 'Low Salary' AS category
-    UNION
-    SELECT 'Average Salary'
-    UNION
-    SELECT 'High Salary'
-),
-cte2 AS (
-    SELECT
-        account_id,
-        IF(income < 20000, 'Low Salary', IF(income <= 50000, 'Average Salary', 'High Salary')) AS category
-    FROM accounts
-)
 SELECT
-    category,
-    COUNT(account_id) AS accounts_count
-FROM cte1
-LEFT JOIN cte2
-    USING (category)
-GROUP BY category
-
+    'Low Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM accounts
+WHERE income < 20000
+UNION
+SELECT
+    'Average Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM accounts
+WHERE income BETWEEN 20000 AND 50000
+UNION
+SELECT
+    'High Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM accounts
+WHERE income > 50000
