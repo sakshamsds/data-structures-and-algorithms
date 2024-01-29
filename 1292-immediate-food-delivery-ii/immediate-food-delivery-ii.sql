f7b666d42dd31826614b30ecb3d65ad8b5a47e43
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-28 20:50:18 -0500

    Time: 1055 ms (68.57%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -6,16 +6,9 @@ WITH cte AS (
         MIN(order_date) AS order_date
     FROM delivery
     GROUP BY customer_id
-),
-cte2 AS (
-    SELECT 
-        customer_id,
-        order_date,
-        customer_pref_delivery_date
-    FROM cte
-    INNER JOIN delivery
-        USING (customer_id, order_date)
 )
-SELECT
-    ROUND(AVG(IF(order_date = customer_pref_delivery_date, 100, 0)), 2) AS immediate_percentage
-FROM cte2;
+SELECT 
+        ROUND(AVG(IF(cte.order_date = customer_pref_delivery_date, 100, 0)), 2) AS immediate_percentage
+FROM cte
+INNER JOIN delivery
+    USING (customer_id, order_date);
