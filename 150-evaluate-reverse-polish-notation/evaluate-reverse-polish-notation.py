da653cbde556ae21edab7b30b4f29a640960f980
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-01-30 14:41:44 -0500

    Time: 61 ms (88.44%) | Memory: 17.1 MB (72.33%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,22 +1,17 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        operators = '+-*/'
+        operations = {
+            '+': lambda x, y : x + y,
+            '-': lambda x, y : x - y,
+            '*': lambda x, y : x * y,
+            '/': lambda x, y : x / y
+        }
         for token in tokens:
-            if token in operators:
+            if token in operations:
                 num2, num1 = stack.pop(), stack.pop()
-                stack.append(self.operation(num1, num2, token))
+                stack.append(int(operations[token](num1, num2)))
             else:
                 stack.append(int(token))
             # print(stack)
-        return stack[0]
-
-    def operation(self, num1, num2, op):
-        if op == '+':
-            return num1 + num2
-        if op == '-':
-            return num1 - num2
-        if op == '*':
-            return num1 * num2
-        if op == '/':
-            return int(num1 / num2)
+        return stack.pop()
