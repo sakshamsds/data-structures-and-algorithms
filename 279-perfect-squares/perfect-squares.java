6f205482349f3dc478f9797423c818f78ba96dda
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-02-08 17:54:47 -0500

    Time: 31 ms (74.05%) | Memory: 42.7 MB (56.31%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,9 +1,7 @@
 class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            dp[i] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
 
         for (int i = 1; i < n + 1; i++) {
