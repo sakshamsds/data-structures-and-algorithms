0313ff616d686156d400181d3a92b8f3e7365ae4
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-02-07 19:26:23 -0500

    Time: 2574 ms (52.20%) | Memory: 16.7 MB (81.54%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -1,17 +1,10 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        cache = {0: 0}
-
-        def dfs(rem):
-            if rem in cache:
-                return cache[rem]
-            min_sqrs = float('inf')
-            for num in range(1, rem + 1):
-                sqr = num * num
-                if sqr > n:
+        # O(n * sqrt(n))
+        dp = [0] + [float('inf')] * n
+        for num in range(1, n + 1):
+            for i in range(1, num + 1):
+                if i * i > num:
                     break
-                min_sqrs = min(min_sqrs, 1 + dfs(rem - sqr))
-            cache[rem] = min_sqrs
-            return min_sqrs
-
-        return dfs(n)
+                dp[num] = min(dp[num], dp[num - i * i] + 1)
+        return dp[-1]
