0793022b1836508bc913c088a442936bf54ab0b0
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-02-08 22:20:54 -0500

    Time: 1494 ms (14.74%) | Memory: 164.4 MB (5.03%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,25 +1,19 @@
-'''
-idx     0   1   2   3   4   5
-num     1   2   3   4   6   8
-        1   2   2   3   3   4
-prev    0   0   0   1   1   3
-'''
-
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
-        dp = [(1, -1)] * len(nums)    # longest, prev
-        longest, max_idx = -1, 0
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[i] % nums[j] == 0 and dp[i][0] < 1 + dp[j][0]:
-                    dp[i] = (1 + dp[j][0], j)
-            if dp[i][0] > longest:
-                longest = dp[i][0]
-                max_idx = i
-        # print(dp, i)
-        subset = []
-        while max_idx != -1:
-            subset.append(nums[max_idx])
-            max_idx = dp[max_idx][1]
-        return subset[::-1]
+        cache = {}  # (i, prev) -> []   # 32
+
+        def dfs(i, prev):
+            if i == len(nums):
+                return []
+            if (i, prev) in cache:
+                return cache[(i, prev)]
+            
+            res = dfs(i + 1, prev)  # skip nums[i]
+            if nums[i] % prev == 0:
+                tmp = [nums[i]] + dfs(i + 1, nums[i])   # include nums[i]
+                res = tmp if len(tmp) > len(res) else res
+            
+            cache[(i, prev)] = res
+            return res
+        return dfs(0, 1)
