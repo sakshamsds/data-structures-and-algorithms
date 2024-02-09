3b71712422492224ccbde19764a5b295260567fe
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-02-08 22:09:47 -0500

    Time: 219 ms (65.14%) | Memory: 16.9 MB (65.23%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,28 +1,32 @@
+'''
+idx     0   1   2   3   4   5
+num     1   2   3   4   6   8
+        1   2   2   3   3   4
+prev    0   0   0   1   1   3
+'''
+
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
-        self.res, subset = [], []
-        cache = {}
+        dp = []    # longest, prev
+        for i in range(len(nums)):
+            dp.append((1, i))
 
-        def dfs(largest, idx):
-            if (largest, idx) in cache and cache[(largest, idx)] >= len(subset):
-                return 
-            cache[(largest, idx)] = len(subset)
-            
-            if idx == len(nums):
-                if len(subset) > len(self.res):
-                    self.res = subset.copy()
-                return len(subset)
+        longest, max_idx = -1, 0
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and dp[i][0] < 1 + dp[j][0]:
+                    dp[i] = (1 + dp[j][0], j)
+            if dp[i][0] > longest:
+                longest = dp[i][0]
+                max_idx = i
 
-            # don't add the current num
-            dfs(largest, idx + 1)
+        # print(dp, i)
 
-            # add cur num if statisfies the condition
-            if nums[idx] % largest == 0:
-                subset.append(nums[idx])
-                dfs(nums[idx], idx + 1)
-                subset.pop()
-            return 
-        
-        dfs(1, 0)
-        return self.res
+        # build subset
+        cur, prev = max_idx, dp[max_idx][1]
+        subset = [nums[cur]]
+        while cur != prev:
+            cur, prev = prev, dp[prev][1]
+            subset.append(nums[cur])
+        return subset[::-1]
