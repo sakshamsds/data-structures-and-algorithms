bd5d3d4b4736e621985031390bb120359b8caf0c
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-02-11 16:21:43 -0500

    Time: 776 ms (90.86%) | Memory: 16.9 MB (99.75%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -1,26 +1,23 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        cache = {}
+        ROWS, COLS = len(grid), len(grid[0])
+        dp = [[0] * COLS for _ in range(COLS)]
 
-        def dfs(r, c1, c2):
-            if min(c1, c2) < 0 or max(c1, c2) == cols or r == rows:
-                return 0 
-            
-            if (r, c1, c2) in cache:
-                return cache[(r, c1, c2)]
+        for r in reversed(range(ROWS)):
+            cur_dp = [[0] * COLS for _ in range(COLS)]
+            for c1 in range(COLS - 1):
+                for c2 in range(c1 + 1, COLS):  # doesn't overlap
+                    max_cherries = 0
+                    cherries = grid[r][c1] + grid[r][c2]
+                    for c1_d, c2_d in product([-1, 0, 1], [-1, 0, 1]):
+                        nc1, nc2 = c1 + c1_d, c2 + c2_d
+                        if nc1 < 0 or nc2 == COLS:
+                            continue
+                        max_cherries = max(
+                            max_cherries,
+                            cherries + dp[nc1][nc2]
+                        )
+                    cur_dp[c1][c2] = max_cherries
+            dp = cur_dp
+        return dp[0][COLS - 1]
 
-            cherries = 0
-            for c1_d in [-1, 0, 1]:
-                for c2_d in [-1, 0, 1]:
-                    cherries = max(cherries, 
-                                    dfs(r + 1, c1 + c1_d, c2 + c2_d))
-
-            cherries += grid[r][c1]
-            if c1 != c2:
-                cherries += grid[r][c2]
-
-            cache[(r, c1, c2)] = cherries
-            return cherries
-
-        return dfs(0, 0, cols - 1)
