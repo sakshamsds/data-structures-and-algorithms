7e2e161e3d90c68db2f80fb3e82e73c9df018e69
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-02-12 23:25:44 -0500

    Time: 292 ms (88.63%) | Memory: 18.7 MB (81.99%) - LeetSync

--- 1140-distant-barcodes/distant-barcodes.py
+++ 1140-distant-barcodes/distant-barcodes.py
@@ -1,32 +1,11 @@
-'''
-1 - 4
-2 - 2
-3 - 2
-
-1   2   1   2   1   3   1   3   
-0   1   2   3   4   5   6   7   
-                    l
-'''
-
-
 class Solution:
     def rearrangeBarcodes(self, barcodes: List[int]) -> List[int]:
-        max_heap = []
-        freqs = Counter(barcodes)
-        for bc, freq in freqs.items():
-            heapq.heappush(max_heap, (-freq, bc))
-
-        res = [0] * len(barcodes)
-        even_i, odd_i = 0, 1        # fill even indices first then odd indices
-        while max_heap:
-            freq, bc = heapq.heappop(max_heap)
-            for f in range(-freq):
-                if even_i < len(barcodes):
-                    res[even_i] = bc
-                    even_i += 2
-                else:
-                    res[odd_i] = bc
-                    odd_i += 2
-            # print('res', res)
-        # print(res)
+        i, n = 0, len(barcodes)
+        res = [0] * n
+        for k, v in collections.Counter(barcodes).most_common():
+            for _ in range(v):
+                res[i] = k
+                i += 2
+                if i >= n:
+                    i = 1
         return res
