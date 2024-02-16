35470dd2c0803be238a4987f7f9f5d404f088920
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-02-15 20:16:08 -0500

    Time: 310 ms (96.75%) | Memory: 33.4 MB (84.81%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -1,19 +1,37 @@
 '''
-num -> freq pairs
-5 - 2
+https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/?envType=daily-question&envId=2024-02-16
+
+buckets
+2 - 1
 4 - 1
+1 - 2
+3 - 3
+
+buckets/freq  ->    1       2   3   4   5   6   7
+elements      ->    2,4     1   3
+num_elements  ->    2       1   1
+
+freq = 3, num_ele = 3
+k = 5, how many uniques ceil(4 / 3) = 2 uniques
+
+k  3
 '''
 
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        min_heap = []
         freqs = collections.Counter(arr)
+        buckets = [0] * (len(arr) + 1)
+        uniques = 0
         for num, freq in freqs.items():
-            min_heap.append((freq, num))
-        heapq.heapify(min_heap)
-        while k >= 0 and min_heap:
-            freq, _ = heapq.heappop(min_heap)
-            if k < freq:
-                return len(min_heap) + 1
-            k -= freq
+            buckets[freq] += 1
+            uniques += 1
+
+        for freq, uniq in enumerate(buckets):
+            num_elements = freq * uniq
+            if k < num_elements:  # break condition
+                return uniques - k // freq
+            else:
+                k -= num_elements
+                uniques -= uniq
+
         return 0
