585d4099897e2be92a38179c9003ac8fcbc4d943
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-04-17 21:04:09 -0400

    Time: 47 ms (33.90%) | Memory: 17.8 MB (34.92%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
@@ -1,19 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-        
+        # bottom up doesn't work
+        # top down does
+        # propagate the strings downwards and then compare the results
+
         def dfs(node, cur):
             if not node:
                 return 
+
             cur = chr(ord('a') + node.val) + cur
-            if not node.left and not node.right:
+            if not node.left and not node.right:        # if leaf node
                 return cur
-            left_str = dfs(node.left, cur) if node.left else None
-            right_str = dfs(node.right, cur) if node.right else None
-            if left_str and right_str:
-                return min(left_str, right_str)
-            elif left_str:
-                return left_str
+
+            left = dfs(node.left, cur)
+            right = dfs(node.right, cur)
+
+            if left and right:
+                return min(left, right) 
+            elif left:
+                return left
             else:
-                return right_str
+                return right
 
         return dfs(root, '')
