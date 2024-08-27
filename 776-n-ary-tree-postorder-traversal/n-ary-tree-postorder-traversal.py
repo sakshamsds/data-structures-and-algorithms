b8c4ee5e43d2d1b4789aca7630f9437180cc63a1
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-08-26 20:57:11 -0400

    Time: 32 ms (99.42%) | Memory: 18.3 MB (45.58%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        self.res = []
+
+        def dfs(node):
+            if not node:
+                return
+            for child in node.children:
+                dfs(child)
+            self.res.append(node.val)
+
+        dfs(root)
+        return self.res
+
+
+    
