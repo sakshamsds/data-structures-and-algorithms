dce21f9221596be2e6f7d306f2cffc8a6bd06c06
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-09-06 13:24:19 -0400

    Time: 60 ms (32.21%) | Memory: 17 MB (61.37%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,32 +1,42 @@
 '''
-You're building the graph one edge at a time. 
-However, before adding an edge between u and v, 
-you first check if there already is a path between them, avoiding a cycle.
+adding an edge to a connected component is gonna create a cycle
+uf -> if u, v has same parent, which implies redundant connection
 '''
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        parent = [i for i in range(n + 1)]
+        rank = [1] * (n + 1)    # size of component
 
-        adjList = collections.defaultdict(list)
+        def find(n):
+            p = parent[n]
+            while p != parent[p]:
+                parent[p] = parent[parent[p]]   # path compression
+                p = parent[p]
+            return p
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
 
-        def dfs(u, v):
-            if u == v:
-                return True
+            if p1 == p2:
+                return False
             
-            for nbr in adjList[u]:
-                if nbr not in visited:
-                    visited.add(nbr)
-                    if dfs(nbr, v):
-                        return True
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            return True
 
-            return False
-            
-        res = []
-        for u, v in edges:
-            visited = set([u])
-            if dfs(u, v):
-                res = [u, v]
-            adjList[u].append(v)
-            adjList[v].append(u)
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return [n1, n2]
 
-        return res
+        return [-1, -1]
+
+        
+
+
+        
