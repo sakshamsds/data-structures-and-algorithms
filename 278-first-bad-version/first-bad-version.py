346a6e2505fbca09fd9a621d084f78c82df9e570
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-09-11 17:47:32 -0400

    Time: 33 ms (66.83%) | Memory: 16.4 MB (92.58%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -3,13 +3,11 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-# after exiting the while loop, left is the minimal k​ satisfying the condition function;
         l, r = 1, n
         while l < r:
-            mid = l + (r - l)//2
+            mid = l + (r - l) // 2
             if isBadVersion(mid):
                 r = mid
             else:
                 l = mid + 1
-
         return l
