2e93c6a855fa4b6b7d9f926596102e120bd87211
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-10-24 15:50:54 -0400

    Time: 0 ms (100.00%) | Memory: 16.6 MB (17.69%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -7,19 +7,15 @@
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         
-        def dfs(n1, n2):        # given two nodes, check if kids matches
+        def dfs(n1, n2):        
             if not n1 and not n2:
                 return True
             
-            if not n1 or not n2:
-                return False
-
-            if n1.val != n2.val:
+            if not n1 or not n2 or n1.val != n2.val:
                 return False
 
             return (dfs(n1.left, n2.left) and dfs(n1.right, n2.right)) or \
             (dfs(n1.right, n2.left) and dfs(n1.left, n2.right))
-
 
         return dfs(root1, root2)
         
