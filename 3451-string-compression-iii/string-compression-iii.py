8844daec4bd67ec43cfcd2b40a051622661d497a
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-11-03 20:26:06 -0500

    Time: 122 ms (95.04%) | Memory: 28.4 MB (8.56%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -1,22 +1,22 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        res = []    
+        comp = []    
         prev, freq = '', 0
         for c in word:
             if c == prev:
+                if freq == 9:
+                    comp.append('9')
+                    comp.append(c)
+                    freq = 0
                 freq += 1
             else:
-                res.append(('9' + prev) * (freq // 9))
-                rem = freq % 9
-                if rem > 0:
-                    res.append(str(freq % 9) + prev)
+                comp.append(str(freq))
+                comp.append(prev)
                 freq = 1
             prev = c
 
-        res.append(('9' + prev) * (freq // 9))
-        rem = freq % 9
-        if rem > 0:
-            res.append(str(freq % 9) + prev)
-        return ''.join(res)
+        comp.append(str(freq))
+        comp.append(prev)
+        return ''.join(comp)[1:]
 
         
