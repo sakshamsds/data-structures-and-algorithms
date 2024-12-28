02101ccbc0b338e54568701436344fac631d8b1c
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-12-27 22:29:07 -0500

    Time: 48 ms (94.75%) | Memory: 23.4 MB (6.68%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -1,16 +1,10 @@
-'''
-            0   1   2   3   4
-            8   1   5   2   6
-v[i] + i    8   2   7   5   10
-best left   8   8   8   8   10
-
-v[j] - j    8   0   3   -1  2
-'''
-
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        best_left, mx_score = -1, -1e5
+        i, mx_score = 0, 0
+
         for j in range(1, len(values)):
-            best_left = max(best_left, values[j - 1] + j - 1)
-            mx_score = max(mx_score, best_left + values[j] - j)
+            mx_score = max(mx_score, values[i] + i + values[j] - j)
+            if values[j] + j > values[i] + i:
+                i = j
+
         return mx_score
