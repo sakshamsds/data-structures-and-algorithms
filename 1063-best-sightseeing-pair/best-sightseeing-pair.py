56ecf2c88338e7527ae4134fe96060123fd3990a
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-12-27 22:26:32 -0500

    Time: 85 ms (19.57%) | Memory: 23.3 MB (21.48%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -2,21 +2,15 @@
             0   1   2   3   4
             8   1   5   2   6
 v[i] + i    8   2   7   5   10
+best left   8   8   8   8   10
 
 v[j] - j    8   0   3   -1  2
-mx          8   3   3   2   2
-
 '''
 
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        suffix = [v - j for j, v in enumerate(values)]
-
-        for j in range(len(suffix) - 2, -1, -1):
-            suffix[j] = max(suffix[j + 1], suffix[j])
-        
-        mx_score = -1e5
-        for i in range(0, len(values) - 1):
-            mx_score = max(mx_score, values[i] + i + suffix[i + 1])
-
+        best_left, mx_score = -1, -1e5
+        for j in range(1, len(values)):
+            best_left = max(best_left, values[j - 1] + j - 1)
+            mx_score = max(mx_score, best_left + values[j] - j)
         return mx_score
