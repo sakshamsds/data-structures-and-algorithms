cef17d6d914b2c7ec4e27abb573ef648833e0488
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-02-08 20:50:58 -0500

    Time: 87 ms (46.19%) | Memory: 38.7 MB (88.57%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -1,25 +1,28 @@
 '''
-idxs - 0 1 2 3
-nums - 4 1 3 3
-diff - 4 0 1 0
-total_pairs = 4 * 3 // 2 = 6
-good_pairs = 2c2 = 1
-bad_pairs = 4
+    4   1   3   3
+    0   1   2   3
+    4   0   1   0
 
-nums[j] - j == nums[i] - i
+1
+6 - 1
+
+3 * 2 - 1
+
+0 - 2
+4 - 1
+1 - 1
+
 '''
 
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        n = len(nums)
-        total_pairs = n * (n - 1) // 2
-        good_pairs = 0
-
         diffs = collections.defaultdict(int)
         for i, num in enumerate(nums):
             diffs[num - i] += 1
 
-        for _, v in diffs.items():
-            good_pairs += v * (v - 1) // 2
+        n = len(nums)
+        total = n * (n - 1) // 2
+        for d in diffs.values():
+            total -= d * (d - 1) // 2
 
-        return total_pairs - good_pairs
+        return total
