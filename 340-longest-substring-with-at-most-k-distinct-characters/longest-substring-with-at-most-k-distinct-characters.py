3dd013ca01e2cd447e1ec0744010b51cd8c6f9a6
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-02-12 15:39:19 -0500

    Time: 35 ms (33.07%) | Memory: 18 MB (49.24%) - LeetSync

--- 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.py
+++ 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.py
@@ -8,12 +8,12 @@ class Solution:
         for right in range(n):
             counter[s[right]] += 1
             
-            while len(counter) > k: 
+            if len(counter) > k:        # if valid
                 counter[s[left]] -= 1
-                if counter[s[left]] == 0:
-                    del counter[s[left]]
+                if not counter[s[left]]:
+                    counter.pop(s[left])
                 left += 1
-            
+
             max_size = max(max_size, right - left + 1)
                     
         return max_size
