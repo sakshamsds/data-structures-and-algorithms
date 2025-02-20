1f9abd8dfa8f294f5c5564b1bb9cb9ef72975c89
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-02-19 23:41:24 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.03%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,11 +1,11 @@
-# Cantor's diagonal argument
-
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        # make the res differ from every num in nums by 1 place
-        res = []
-        for i in range(len(nums)):
-            cur = nums[i][i]
-            res.append("1" if cur == "0" else "0")
-        return "".join(res)
-        
+        # start with zeros and make string different from every num
+        n = len(nums)
+        unique = ['0'] * n
+
+        for i, num in enumerate(nums):
+            if num[i] == unique[i]:
+                unique[i] = '1'
+
+        return ''.join(unique) 
