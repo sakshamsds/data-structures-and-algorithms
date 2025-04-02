1210223b42a0471690cdf803bf4086e2a20fe55c
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-04-01 22:54:20 -0400

    Time: 7 ms (40.21%) | Memory: 17.7 MB (57.76%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -5,14 +5,11 @@ class Solution:
             last_idx[ord(c) - ord('a')] = i
 
         sizes = []
-        cur_chars = set()
         max_idx, start = -1, 0
         for i, c in enumerate(s):
-            cur_chars.add(c)
             max_idx = max(max_idx, last_idx[ord(c) - ord('a')])            
 
             if i == max_idx:
-                cur_chars = set()
                 sizes.append(max_idx - start + 1)
                 start = i + 1
 
