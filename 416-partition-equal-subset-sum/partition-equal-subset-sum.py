7148024f8297dfa72c385270b5ec6a9dc64ea332
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-04-06 20:28:25 -0400

    Time: 2453 ms (10.10%) | Memory: 576.9 MB (5.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,33 +1,26 @@
 '''
-        0   1   2   3   4   5   6   7   8   9   10  11
-0       T   F   F   F   F   F   F   F   F   F   F   F
-1       T   T   F   F   F   F   F   F   F   F   F   F
-5       T   T   F   F   F   T   T   F   F   F   F   F
-11      T   T   F   F   F   T   T   F   F   F   F   T
-5       T   T   F   F   F   T   T   F   F   F   F   T
-
-        0   1   2   3   4
-0       T   F   F   F   F
-1       T   T   F   F   F
-2       T   T   T   T   F
-5       T   T   T   T   F
+        1   5   11  5
 '''
-
-
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # can we make target given the nums, variant of coin change, 0/1 knapsack
         total = sum(nums)
         if total % 2 == 1:
             return False
-        target = total // 2
+        cache = {}
 
-        dp = [True] + [False] * target
-        for num in nums:
-            for t in range(target, num - 1, -1):
-                dp[t] = dp[t] or dp[t - num]
-            if dp[target]:
+        def dfs(i, rem):
+            if rem == 0:
                 return True
 
-        return dp[target]
+            if i == len(nums):
+                return False
+
+            key = (i, rem)
+            if key in cache:
+                return cache[key]
+
+            cache[key] = dfs(i + 1, rem) or dfs(i + 1, rem - nums[i])
+            return cache[key]
+
+        return dfs(0, total // 2)
