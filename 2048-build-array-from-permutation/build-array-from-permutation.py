783e5249fc38f7240a7e39f30eddc10dc4280f00
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-05-05 21:03:56 -0400

    Time: 7 ms (5.86%) | Memory: 17.9 MB (55.24%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -0,0 +1,12 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        for i in range(n):
+            nums[i] += 1000 * (nums[nums[i]] % 1000)
+
+        # print(nums)
+        for i in range(n):
+            nums[i] //= 1000
+
+        return nums
