e27954e0052c278fb7d91166938d7e7e6a2b0201
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-06-11 16:59:51 -0400

    Time: 291 ms (41.36%) | Memory: 28.3 MB (94.36%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
@@ -1,16 +1,17 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        # sliding window approach
-        # number we trying to make is the last number in the window
+        # try to make the rightmost element in the window
+
         nums.sort()
-        res, l, window_sum = 0, 0, 0
+        l, res, window_sum = 0, 0, 0
 
         for r in range(len(nums)):
             window_sum += nums[r]
-            # condition: sum of window + k >= size of window * max_num
-            # editorial: we will not shrink the window - we will just try to grow it as large as we can
-            if window_sum + k < (r - l + 1) * nums[r]:
+
+            while (r - l + 1) * nums[r] > window_sum + k:
                 window_sum -= nums[l]
                 l += 1
 
-        return r - l + 1
+            res = max(res, r - l + 1)
+        
+        return res
