8c4c11c7d641fcb0a23cd5c993562a552a13a0ee
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-07-22 14:47:58 -0400

    Time: 126 ms (98.90%) | Memory: 28.8 MB (50.60%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -1,26 +1,18 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        prefix_sum = [nums[0]]
-        for i in range(1, len(nums)):
-            prefix_sum.append(nums[i] + prefix_sum[-1])
+        l, cur_score, max_score = 0, 0, 0
+        uniques = set()
 
-        indices_map = collections.defaultdict(int)      # window num -> idx mapping
-        indices_map[nums[0]] = 0
+        for num in nums:
+            if num not in uniques:
+                uniques.add(num)
+                cur_score += num
+                max_score = max(max_score, cur_score)
+            else:
+                while nums[l] != num:
+                    uniques.remove(nums[l])
+                    cur_score -= nums[l]
+                    l += 1
+                l += 1
 
-        l, mx_score = 0, nums[0]
-        for r in range(1, len(nums)):
-            cur = nums[r]
-            if cur in indices_map:
-                l = max(l, indices_map[cur] + 1)
-            # if cur in indices_map:
-            #     if indices_map[cur] < l:
-            #         indices_map.pop(cur)
-            #     else:
-            #         l = indices_map[cur] + 1
-            indices_map[cur] = r            
-            # print(l, r, prefix_sum[r] - prefix_sum[l] + nums[l])
-            mx_score = max(mx_score, prefix_sum[r] - prefix_sum[l] + nums[l])
-
-        return mx_score
-
-
+        return max_score
