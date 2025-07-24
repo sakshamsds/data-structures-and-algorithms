0a78f7d151d69422753034fa6ac94409401a0a0a
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-07-24 15:25:55 -0400

    Time: 111 ms (16.94%) | Memory: 17.8 MB (37.64%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,20 @@
+'''
+    1   2   3   4   5   6   7   8   9   10  11  12
+    y       y                       y
+'''
+
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        # available
+        powers = [3 ** i for i in range(20)]
+
+        def dfs(i, num):
+            if num == 0:
+                return True
+
+            if num < powers[i]:
+                return False
+
+            return dfs(i + 1, num) or dfs(i + 1, num - powers[i])
+
+        return dfs(0, n)
