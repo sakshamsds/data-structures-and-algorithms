4febbeac9bd9c205bf35b5f3b420afdd1f76ac37
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-08-05 19:10:36 -0400

    Time: 230 ms (7.41%) | Memory: 27.2 MB (9.26%) - LeetSync

---
+++ 1757-minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+        # bfs
+
+        # nbrs +a, -b
+        # cannot jump backwards twice
+        # sanity check, cannot be negative
+        # start = 0
+
+        # target = x
+
+        q = collections.deque([(0, False)])
+        forbidden = set(forbidden)
+        visited = set((0, False))       # number, last_backward
+        min_jumps = 0
+
+        while q:
+            num_nbrs = len(q)
+            for _ in range(num_nbrs):
+                cur, last_backward = q.popleft()
+                if cur == x:
+                    return min_jumps
+
+                nbrs = [(cur + a, False)]
+                if not last_backward:
+                    nbrs.append((cur - b, True))
+
+                for nbr, is_backward in nbrs[::-1]:
+                    if (
+                        nbr < 0 or
+                        nbr > (x + 20 * b) or
+                        (nbr, is_backward) in visited or
+                        nbr in forbidden
+                    ):
+                        continue
+                    q.append((nbr, is_backward))
+                    visited.add((nbr, is_backward))
+            min_jumps += 1
+
+        return -1
