a809379c2bda505011a75fcc6be648cfb45a1f43
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-12-07 21:54:03 -0500

    Time: 3 ms (97.13%) | Memory: 18.2 MB (66.78%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.py
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.py
@@ -1,19 +1,4 @@
-'''
-    R   L   R   S   L   L
-'''
-
 class Solution:
     def countCollisions(self, directions: str) -> int:
-        
-        def get_collisions(traversal, stationary, other_dir):
-            can_collide = False
-            collisions = 0
-            for d in directions[::traversal]:
-                if d == stationary or d == "S":
-                    can_collide = True
-
-                if d == other_dir and can_collide:
-                    collisions += 1
-            return collisions
-        
-        return get_collisions(1, "R", "L") + get_collisions(-1, "L", "R") 
+        dirs = directions.lstrip("L").rstrip("R")
+        return len(dirs) - dirs.count("S")
