e3a38f1bf00244daccdba10a5e8eb575e6d43b41
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-12-29 17:44:28 -0500

    Time: 4738 ms (26.26%) | Memory: 17.2 MB (99.49%) - LeetSync

--- 757-pyramid-transition-matrix/pyramid-transition-matrix.py
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.py
@@ -5,27 +5,17 @@ class Solution:
         for pattern in allowed:
             mapping[pattern[:2]].append(pattern[2])
 
-        def backtrack(i_cur, cur_row, upper_row):
-            # print(i_cur, cur_row, upper_row)
+        def backtrack(i, cur_row, upper_row):
             if len(cur_row) == 1:
                 return True
 
-            if i_cur == len(cur_row) - 1:
-                return backtrack(0, upper_row, list())
+            if i == len(cur_row) - 1:
+                return backtrack(0, upper_row, "")
 
-            cur_key = cur_row[i_cur] + cur_row[i_cur + 1]
-            if cur_key not in mapping:
-                return False
-
-            for top in mapping[cur_key]:
-                upper_row.append(top)
-                if backtrack(i_cur + 1, cur_row, upper_row):
+            key = cur_row[i:i + 2]
+            for top in mapping[key]:
+                if backtrack(i + 1, cur_row, upper_row + top):
                     return True
-                upper_row.pop()
-
-            if len(upper_row) > 0:
-                backtrack(0, upper_row, list())
-
             return False
 
-        return backtrack(0, list(bottom), list())
+        return backtrack(0, bottom, "")
