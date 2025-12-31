c6f5a20fee5f89c9b8ba3b2c34cc47f3d074b7d5
Sakshamdeep Singh <saksham.sds@gmail.com>
2025-12-31 15:31:20 -0500

    Time: 0 ms (100.00%) | Memory: 17.2 MB (98.62%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,20 +1,16 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        # decide divisor using prefix, long to short
         len1, len2 = len(str1), len(str2)
 
-        for prefix_len in range(min(len1, len2), 0, -1):
-            prefix = str1[:prefix_len]
+        def isDivisor(end):
+            if len1 % end != 0 or len2 % end != 0:
+                return False
+            freq1, freq2 = len1 // end, len2 // end
+            prefix = str1[:end]
+            return prefix * freq1 == str1 and prefix * freq2 == str2
 
-            # check 1
-            if len1 % prefix_len != 0 or len2 % prefix_len != 0:
-                continue
-            
-            # check 2
-            q1, q2 = len1 // prefix_len, len2 // prefix_len
-            if prefix * q1 != str1 or prefix * q2 != str2:
-                continue
-
-            return prefix
+        for end in range(min(len1, len2), 0, -1):
+            if isDivisor(end):
+                return str1[:end]
 
         return ""
