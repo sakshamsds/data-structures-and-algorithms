1b58a82ddfd9d2cfa36e81caa641d4679ff77c47
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-07 17:08:40 -0500

    Time: 3 ms (75.42%) | Memory: 20.5 MB (5.11%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -2,26 +2,50 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         # preq -> course, b -> a
 
-        adj_list = collections.defaultdict(list)
-        indegrees = [0] * numCourses
-        for course, preq in prerequisites:
-            indegrees[course] += 1
-            adj_list[preq].append(course)
+        # adj_list = collections.defaultdict(list)
+        # indegrees = [0] * numCourses
+        # for course, preq in prerequisites:
+        #     indegrees[course] += 1
+        #     adj_list[preq].append(course)
 
-        q = collections.deque()
-        for course, indegree in enumerate(indegrees):
-            if indegree == 0:
-                q.append(course)
+        # q = collections.deque()
+        # for course, indegree in enumerate(indegrees):
+        #     if indegree == 0:
+        #         q.append(course)
+
+        # topo_sort = []
+        # while q:
+        #     preq = q.popleft()
+        #     for course in adj_list[preq]:
+        #         indegrees[course] -= 1
+        #         if indegrees[course] == 0:
+        #             q.append(course)
+        #     topo_sort.append(preq)
+
+        # if sum(indegrees) == 0:
+        #     return topo_sort
+        # return []
+
+        adj_list = collections.defaultdict(list)
+        indegree = [0] * numCourses                 # indegree for given courses
+        for course, preq in prerequisites:
+            indegree[preq] += 1
+            adj_list[course].append(preq)
+
+        q = collections.deque()                     # queue for processing
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
 
         topo_sort = []
         while q:
-            preq = q.popleft()
-            for course in adj_list[preq]:
-                indegrees[course] -= 1
-                if indegrees[course] == 0:
-                    q.append(course)
-            topo_sort.append(preq)
+            cur = q.popleft()
+            for nbr in adj_list[cur]:               # remove indegree for the nbrs 
+                indegree[nbr] -= 1
+                if indegree[nbr] == 0:
+                    q.append(nbr)
+            topo_sort.append(cur)
 
-        if sum(indegrees) == 0:
-            return topo_sort
+        if sum(indegree) == 0:
+            return topo_sort[::-1]
         return []
