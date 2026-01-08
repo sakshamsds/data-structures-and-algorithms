7ec43d1297f6c0b3e94af186f8122c1197fe45ab
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-07 20:56:04 -0500

    Time: 6 ms (92.18%) | Memory: 21.5 MB (10.68%) - LeetSync

---
+++ 1101-parallel-courses/parallel-courses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        adj_list = collections.defaultdict(list)
+        indegrees = [0] * n
+        for prev, nxt in relations:
+            prev, nxt = prev - 1, nxt - 1
+            indegrees[nxt] += 1          
+            adj_list[prev].append(nxt)
+
+        q = collections.deque()
+        for course, indegree in enumerate(indegrees):
+            if indegree == 0:
+                q.append(course)
+
+        num_semesters = 0
+        while q:
+            num_semesters += 1
+            for _ in range(len(q)):         # take all in current semester
+                course = q.popleft()
+                for nxt in adj_list[course]:
+                    indegrees[nxt] -= 1
+                    if indegrees[nxt] == 0:
+                        q.append(nxt)
+
+        if sum(indegrees) == 0:
+            return num_semesters
+        return -1
