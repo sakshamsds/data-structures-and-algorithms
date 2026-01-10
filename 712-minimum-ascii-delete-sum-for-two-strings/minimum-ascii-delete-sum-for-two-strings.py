5d6e8b0bba1c89c43bf94042d83bf4bd25316aec
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-10 15:06:59 -0500

    Time: 885 ms (11.80%) | Memory: 239.7 MB (5.14%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -12,11 +12,16 @@ i, j
 delete i, i + 1
 delete j, j + 1
 don't delete only if equal, i + 1, j + 1
+
+
+
 '''
+
 
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        cache = {}
+
+        @cache
         def dfs(i, j):
             if i == len(s1) or j == len(s2):
                 deleted = 0
@@ -26,13 +31,9 @@ class Solution:
                     deleted += ord(s2[k])
                 return deleted
 
-            if (i, j) in cache:
-                return cache[(i, j)]
-
             min_del = min(ord(s1[i]) + dfs(i + 1, j), ord(s2[j]) + dfs(i, j + 1))
             if s1[i] == s2[j]:
                 min_del = min(min_del, dfs(i + 1, j + 1))
 
-            cache[(i, j)] = min_del
             return min_del
         return dfs(0, 0)
