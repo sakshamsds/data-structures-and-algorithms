73365a1a3e8c31379aca3b80e2b5e49d4724f9ec
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-11 18:05:06 -0500

    Time: 51 ms (99.58%) | Memory: 33.3 MB (43.42%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,40 +1,20 @@
+'''
+    dp[i, j] = maximum square length ending at that (i, j)
+    dp[i, j] = min(dp[i - 1, j], dp[i, j - 1], dp[i - 1, j - 1]) if (i, j) has 1
+'''
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-
-        prefix = [[0] * cols for _ in range(rows)]
-        prefix[0][0] = int(matrix[0][0])
+        prev_dp = [0] + [int(num) for num in matrix[0]]
+        mx_sq = max(prev_dp)
 
         for r in range(1, rows):
-            prefix[r][0] = prefix[r - 1][0] + int(matrix[r][0])
-        for c in range(1, cols):
-            prefix[0][c] = prefix[0][c - 1] + int(matrix[0][c])
-        for r in range(1, rows):
-            for c in range(1, cols):
-                prefix[r][c] = int(matrix[r][c]) + prefix[r][c - 1] + \
-                                prefix[r - 1][c] - prefix[r - 1][c - 1]
-
-        # for row in prefix:
-            # print(row)
-
-        largest = 0
-        # traverse all diagonals for all numbers
-        for r in range(rows):
+            dp = [0] * (cols + 1)
             for c in range(cols):
-                # print(r, c)
-                # print('----')
-                # find max in that diagonal
-                top_r, top_c = r - 1, c - 1
-                while top_r >= -1 and top_c >= -1:
-                    top_left = prefix[top_r][top_c] if top_r >= 0 and top_c >= 0 else 0
-                    top_right = prefix[top_r][c] if top_r >= 0 else 0
-                    bottom_left = prefix[r][top_c] if top_c >= 0 else 0
-                    area_sum = prefix[r][c] + top_left - top_right - bottom_left
-                    # print(area_sum, (r - top_r) ** 2)
-                    if area_sum == (r - top_r) ** 2:    # square matrix exist
-                        largest = max(largest, area_sum)
-                    top_r, top_c = top_r - 1, top_c - 1
-                # print()
+                if matrix[r][c] == '0':
+                    continue
+                dp[c + 1] = min(dp[c], prev_dp[c], prev_dp[c + 1]) + 1
+                mx_sq = max(mx_sq, dp[c + 1])
+            prev_dp = dp
 
-        return largest
-
+        return mx_sq ** 2
