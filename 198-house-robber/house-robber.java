a8bb4bdc87101cf869a104d63f3b5a3b2ec6ab9f
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-16 16:56:17 -0500

    Time: 0 ms (100.00%) | Memory: 42.8 MB (62.46%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,14 +1,21 @@
+/*
+    1       2           3           1
+    nr      nr+2        
+    r       (r, nr)
+
+*/
+
 class Solution {
     public int rob(int[] nums) {
+        int no_rob = 0;
         int rob = 0;
-        int noRob = 0;
 
         for (int num : nums) {
-            int temp = rob;
-            rob = noRob + num;
-            noRob = Math.max(temp, noRob);
+            int tmp = no_rob;
+            no_rob = Math.max(no_rob, rob);
+            rob = tmp + num;
         }
 
-        return Math.max(rob, noRob);
+        return Math.max(no_rob, rob);
     }
 }
