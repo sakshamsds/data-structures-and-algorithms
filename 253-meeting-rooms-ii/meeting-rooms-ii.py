2731ead9a86060897eac4c8d6c2baca2f5667c85
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-16 23:40:24 -0500

    Time: 3 ms (86.60%) | Memory: 21.3 MB (22.21%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        prev_ends = []
+        max_rooms = 1
+
+        for s, e in intervals:
+            new_ends = []
+            for pe in prev_ends:
+                if s < pe:
+                    new_ends.append(pe)
+            new_ends.append(e)
+            prev_ends = new_ends
+            max_rooms = max(max_rooms, len(prev_ends))
+
+        return max_rooms
+        
