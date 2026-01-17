da16034f2f27d0a0c74409dea7c357349cd33568
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-16 23:47:51 -0500

    Time: 3 ms (86.60%) | Memory: 21.5 MB (18.98%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,17 +1,12 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x:x[0])
-        prev_ends = []
-        max_rooms = 1
+        min_heap = [-1]       # end times
 
         for s, e in intervals:
-            new_ends = []
-            for pe in prev_ends:
-                if s < pe:
-                    new_ends.append(pe)
-            new_ends.append(e)
-            prev_ends = new_ends
-            max_rooms = max(max_rooms, len(prev_ends))
+            if min_heap[0] <= s:         # assign to the room where meeting has ended
+                heapq.heappop(min_heap)
 
-        return max_rooms
-        
+            heapq.heappush(min_heap, e)
+
+        return len(min_heap)
