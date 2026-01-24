7c5f615b881c14fee8e3306fa9064c362cc08b4c
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-24 02:44:26 -0500

    Time: 20 ms (15.69%) | Memory: 26.1 MB (11.29%) - LeetSync

--- 359-logger-rate-limiter/logger-rate-limiter.py
+++ 359-logger-rate-limiter/logger-rate-limiter.py
@@ -1,13 +1,22 @@
 class Logger:
 
     def __init__(self):
-        self.store = {}        # msg -> ts
+        self.uniques = set()
+        self.q = collections.deque()
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        if message not in self.store or timestamp - self.store[message] >= 10:
-            self.store[message] = timestamp
+        while self.q and timestamp - self.q[0][0] >= 10:
+            _, msg = self.q.popleft()
+            self.uniques.remove(msg)
+
+        if message not in self.uniques:
+            self.uniques.add(message)
+            self.q.append((timestamp, message))
             return True
+
         return False
+
+
 
 
 # Your Logger object will be instantiated and called as such:
