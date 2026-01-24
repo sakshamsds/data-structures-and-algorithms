312775de99cfda389b2c4103b77482f9be1bc594
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-24 16:04:23 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (61.94%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -1,43 +1,27 @@
 class HitCounter(object):
 
     def __init__(self):
-        self.ts = []
+        self.q = collections.deque()
+
+    def remove(self, ts):
+        while self.q and ts - self.q[0] >= 300:
+            self.q.popleft()
 
     def hit(self, timestamp):
         """
         :type timestamp: int
         :rtype: None
         """
-        self.ts.append(timestamp)
+        self.remove(timestamp)
+        self.q.append(timestamp)
 
     def getHits(self, timestamp):
         """
         :type timestamp: int
         :rtype: int
         """
-        if not self.ts:
-            return 0
-
-        # reject the higher ts
-        l, r = 0, len(self.ts)
-        while l < r:
-            mid = l + (r - l) // 2
-            if timestamp < self.ts[mid]:
-                r = mid
-            else:
-                l = mid + 1
-
-        idx = max(0, l - 1)
-        cnt_hits = 0
-        min_ts = timestamp - 300        # [t - 300, t]
-        for i in range(idx, -1, -1):
-            if self.ts[i] <= min_ts:
-                break
-            cnt_hits += 1
-
-        return cnt_hits
-
-        
+        self.remove(timestamp)
+        return len(self.q)        
 
 
 # Your HitCounter object will be instantiated and called as such:
