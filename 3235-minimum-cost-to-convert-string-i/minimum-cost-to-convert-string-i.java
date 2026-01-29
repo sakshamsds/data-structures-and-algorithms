98089ab246c631d18e0cbead89e181323b751c44
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-01-29 18:40:05 -0500

    Time: 23 ms (61.65%) | Memory: 47.8 MB (80.65%) - LeetSync

---
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -0,0 +1,58 @@
+/*
+b -> c = 10
+b -> a -> c = 5
+
+Floyd Warshall Algorithm
+    a   b   c   d   e
+a   0   2   
+b       0   5    
+c       5   0       1
+d               0   20
+e       2           0
+
+abcd
+acbe
+*/
+
+class Solution {
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        long[][] dist = new long[26][26];
+        for (int r = 0; r < 26; r++) {
+            for (int c = 0; c < 26; c++) {
+                dist[r][c] = (r == c) ? 0 : Integer.MAX_VALUE;
+            }
+        }
+
+        int n = source.length();        // vertices
+        int num_edges = original.length;        // edges
+        for (int i = 0; i < num_edges; i++) {
+            int src = original[i] - 'a';
+            int dst = changed[i] - 'a';
+            dist[src][dst] = Math.min(dist[src][dst], (long) cost[i]);
+        }
+
+        // Floyd Warshall
+        for (int k = 0; k < 26; k++) {
+            for (int src = 0; src < 26; src++) {
+                for (int dst = 0; dst < 26; dst++) {
+                    if (dist[src][dst] > dist[src][k] + dist[k][dst]) {
+                        dist[src][dst] = dist[src][k] + dist[k][dst];
+                    }
+                }
+            }
+        }
+
+        long min_cost = 0;
+        for (int i = 0; i < n; i++) {
+            int src = source.charAt(i) - 'a';
+            int dst = target.charAt(i) - 'a';
+            if (dist[src][dst] >= Integer.MAX_VALUE) {
+                return -1;
+            }
+            min_cost += dist[src][dst];
+        }
+
+        // System.out.println(Arrays.deepToString(dist));
+        return min_cost;
+    }
+}
