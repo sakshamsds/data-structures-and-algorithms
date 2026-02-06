5676745ac014f327e81deeda14a8d87b5319c29c
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-02-06 00:07:42 -0500

    Time: 5 ms (27.31%) | Memory: 46.7 MB (60.61%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,60 @@
+class Solution {
+    private List<List<String>> solutions;
+    private Set<Integer> cols;
+    private Set<Integer> pos_diag;
+    private Set<Integer> neg_diag;
+    private int N;
+    private char[][] board;
+
+    public List<List<String>> solveNQueens(int n) {
+        solutions = new ArrayList<>();
+        cols = new HashSet<>();
+        pos_diag = new HashSet<>();
+        neg_diag = new HashSet<>();
+        N = n;
+        board = new char[n][n];
+        // fill in the board
+        for (int r = 0; r < n; r++){
+            for (int c = 0; c < n; c++) {
+                board[r][c] = '.';
+            }
+        }
+        backtrack(0);
+        return solutions;        
+    }
+
+    private List<String> createBoard() {
+        List<String> output = new ArrayList<>();
+        for (int row = 0; row < N; row++) {
+            String row_string = new String(board[row]);
+            output.add(row_string);
+        }
+        return output;
+    }
+
+    private void backtrack(int row) {
+        if (row == N) {
+            solutions.add(createBoard());
+            return;
+        }
+
+        for (int col = 0; col < N; col++) {
+            if (cols.contains(col) || 
+                pos_diag.contains(row - col) || 
+                neg_diag.contains(row + col)
+            ) {
+                continue;
+            }
+            cols.add(col);
+            pos_diag.add(row - col);
+            neg_diag.add(row + col);
+            board[row][col] = 'Q';
+            backtrack(row + 1);
+            board[row][col] = '.';
+            cols.remove(col);
+            pos_diag.remove(row - col);
+            neg_diag.remove(row + col);
+        }
+
+    }
+}
