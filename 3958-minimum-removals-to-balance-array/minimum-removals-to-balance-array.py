1b86bd0730b52a8a8cf8a5f789c8f888a953b74d
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-02-05 22:30:25 -0500

    Time: 76 ms (98.19%) | Memory: 34.6 MB (28.52%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
@@ -7,11 +7,9 @@ l   r
 class Solution:
     def minRemoval(self, nums: List[int], k: int) -> int:
         nums.sort()
-        l, max_window = 0, 0
-
+        l = 0
         for r in range(len(nums)):
-            while nums[l] * k < nums[r]:
+            if nums[l] * k < nums[r]:
                 l += 1
-            max_window = max(max_window, r - l + 1)
 
-        return len(nums) - max_window
+        return l
