17787fbe125dc3893932c372c3bca4cb33a3381b
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-02-07 17:49:01 -0500

    Time: 37 ms (43.39%) | Memory: 48 MB (16.80%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,38 @@
+/*
+    aabaabbab
+    aa aabb b
+    left a
+    right b
+    a   a   b   a   a   b   b   a   b
+0a  5a
+0b  4b
+    1a  4a
+    0b  4b
+                    4a  1a
+                    1b  3b
+*/
+
+class Solution {
+    public int minimumDeletions(String s) {
+        int suffix_a = 0;
+        for (char c : s.toCharArray()) {
+            suffix_a += (c == 'a') ? 1 : 0;
+        }
+        int suffix_b = s.length() - suffix_a;
+        int prefix_a = 0;
+        int prefix_b = 0;
+
+        int min_deletions = prefix_b + suffix_a;
+        for (char c : s.toCharArray()) {
+            if (c == 'a') {
+                suffix_a--;
+                prefix_a++;
+            } else {
+                suffix_b--;
+                prefix_b++;
+            }
+            min_deletions = Math.min(min_deletions, prefix_b + suffix_a);
+        }
+        return min_deletions;        
+    }
+}
