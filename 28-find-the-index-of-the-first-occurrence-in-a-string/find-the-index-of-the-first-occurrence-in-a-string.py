9629fe45c1f22291958408b3758284e8128284e2
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-02-12 14:40:27 -0500

    Time: 5 ms (6.38%) | Memory: 19.4 MB (39.44%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,9 +1,34 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         m, n = len(haystack), len(needle)
+        if m < n:
+            return -1
 
-        for i in range(m - n + 1):
-            if haystack[i:i + n] == needle:
-                return i
+        MOD = 1_000_000_033
+        # MOD = 113
+        BASE = 26     
+        MAX_WEIGHT = 1
+
+        for _ in range(n - 1):
+            MAX_WEIGHT = (MAX_WEIGHT * BASE) % MOD   
+
+        def hash(s):
+            h = 0
+            for c in s:
+                h = (h * BASE + ord(c) - ord('a')) % MOD
+            return h
+
+        needle_hash = hash(needle)
+        window_hash = hash(haystack[:n])
+
+        for l in range(m - n + 1):
+            if window_hash == needle_hash and haystack[l:l + n] == needle:
+                return l
+
+            if l < m - n:
+                first_char_val = ord(haystack[l]) - ord('a')
+                last_char_val = ord(haystack[l + n]) - ord('a')
+                window_hash = (window_hash - first_char_val * MAX_WEIGHT) % MOD
+                window_hash = (window_hash * BASE + last_char_val) % MOD
 
         return -1
