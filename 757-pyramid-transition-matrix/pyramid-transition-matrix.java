6581767df8e5371a6d49342914c29e4c9470f36f
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-02-13 13:53:02 -0500

    Time: 247 ms (46.11%) | Memory: 75.2 MB (5.79%) - LeetSync

--- 757-pyramid-transition-matrix/pyramid-transition-matrix.java
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.java
@@ -1,9 +1,11 @@
 class Solution {
 
     private Map<String, List<Character>> store;
+    private Map<String, Boolean> memo;
 
     public boolean pyramidTransition(String bottom, List<String> allowed) {
         // create mapping one, two -> list(three)
+        memo = new HashMap<>();
         store = new HashMap<>();
         for (String s : allowed) {
             String key = s.substring(0, 2);
@@ -13,7 +15,6 @@ class Solution {
             }
             store.get(key).add(value);
         }
-
         return backtrack(0, bottom, "");
     }
 
@@ -28,11 +29,22 @@ class Solution {
             return true;
         }
 
+        String memoKey = String.valueOf(i) + "," + curStr + "," + nextStr;
+        if (memo.containsKey(memoKey)) {
+            return memo.get(memoKey);
+        }
+        boolean valid = false;
+
         String key = curStr.substring(i, i + 2);
         if (!store.containsKey(key)) return false;
         for (char c : store.get(key)) {
-            if (backtrack(i + 1, curStr, nextStr + c)) return true;
+            if (backtrack(i + 1, curStr, nextStr + c)) {
+                valid = true;
+                break;
+            }
         }
-        return false;
+
+        memo.put(memoKey, valid);
+        return valid;
     }
 }
