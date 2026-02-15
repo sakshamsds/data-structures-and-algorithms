a419f77504ea0b62ca2ac597f0bf95b102b1ce08
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-02-15 17:03:38 -0500

    Time: 2 ms (49.59%) | Memory: 44.4 MB (10.02%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,31 +1,19 @@
-/*
-        11  3
-        11  3
-       110
- */
-
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
         int carry = 0;
-        int i = 0;
+        int ai = a.length() - 1;
+        int bi = b.length() - 1;
 
-        while (i < a.length() || i < b.length()) {
-            int ai = a.length() - 1 - i;
-            int bi = b.length() - 1 - i;
-
-            int val1 = (ai >= 0) ? Character.getNumericValue(a.charAt(ai)) : 0;
-            int val2 = (bi >= 0) ? Character.getNumericValue(b.charAt(bi)) : 0;
-            int digit = val1 + val2 + carry;
-            carry = (digit > 1) ? 1 : 0;
-            digit %= 2;
-            sb.append(String.valueOf(digit));
-            i++;
+        while (ai >= 0 || bi >= 0 || carry > 0) {
+            carry += (ai >= 0) ? Character.getNumericValue(a.charAt(ai)) : 0;
+            carry += (bi >= 0) ? Character.getNumericValue(b.charAt(bi)) : 0;
+            sb.append(String.valueOf(carry % 2));
+            carry /= 2;
+            ai--;
+            bi--;
         }
-
-        if (carry == 1) sb.append(String.valueOf(carry));
-        sb.reverse();
-        return sb.toString();
+        return sb.reverse().toString();
     }
 
 }
