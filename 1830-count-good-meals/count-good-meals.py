b5ad4ed592d58e65f69608ab829be84eb69aecc0
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-02-19 16:44:56 -0500

    Time: 1276 ms (20.15%) | Memory: 76.5 MB (7.07%) - LeetSync

--- 1830-count-good-meals/count-good-meals.py
+++ 1830-count-good-meals/count-good-meals.py
@@ -1,17 +1,14 @@
-'''
-using hint 1, modified two sum
-max sum = 2 ** 22
-'''
-
 class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
-        # store powers of 2
-        powers = [2 ** power for power in range(22)]
-        MOD = 10 ** 9 + 7
-        freq_map = defaultdict(int)
-        good_meals = 0
+        two_powers = [2 ** i for i in range(22)]
+
+        MOD = 1_000_000_007
+        store = collections.defaultdict(int)
+
+        res = 0
         for d in deliciousness:
-            for power in powers:
-                good_meals = (good_meals + freq_map[power - d]) % MOD
-            freq_map[d] += 1
-        return good_meals
+            for power in two_powers:
+               res = (res + store[power - d]) % MOD
+            store[d] += 1 
+
+        return res
