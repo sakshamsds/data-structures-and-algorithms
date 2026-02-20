840cd07628c4479d9f7cbc576421bbc8229239f3
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-02-20 18:19:43 -0500

    Time: 6 ms (26.20%) | Memory: 45.2 MB (10.06%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -5,25 +5,28 @@ class Solution {
     private String s3;
 
     public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) return false;
         this.s1 = s1;
         this.s2 = s2;
         this.s3 = s3;
         dp = new HashMap<>();
-        if (s1.length() + s2.length() != s3.length()) return false;
         return dfs(0, 0, 0);
     }
 
     private boolean dfs(int i, int j, int k) {
         if (k == s3.length()) return true;
 
-        String state = i + "," + j + "," + k;
+        String state = i + "," + j;
         if (dp.containsKey(state)) {
             return dp.get(state);
         }
 
+        if (i == s1.length()) return s2.substring(j).equals(s3.substring(k));
+        if (j == s2.length()) return s1.substring(i).equals(s3.substring(k));
+
         boolean valid = false;
-        if (i < s1.length() && s1.charAt(i) == s3.charAt(k) && dfs(i + 1, j, k + 1)) valid = true;
-        if (j < s2.length() && s2.charAt(j) == s3.charAt(k) && dfs(i, j + 1, k + 1)) valid = true;
+        if (s1.charAt(i) == s3.charAt(k) && dfs(i + 1, j, k + 1)) valid = true;
+        if (s2.charAt(j) == s3.charAt(k) && dfs(i, j + 1, k + 1)) valid = true;
 
         dp.put(state, valid);
         return valid;
