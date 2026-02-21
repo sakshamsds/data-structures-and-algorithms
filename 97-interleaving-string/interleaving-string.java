56d488645a97a13e1701616eb2862edcdd2dc7ed
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-02-20 20:48:23 -0500

    Time: 3 ms (70.78%) | Memory: 43.3 MB (73.27%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,34 +1,52 @@
-class Solution {
-    private Map<String, Boolean> dp ;
-    private String s1;
-    private String s2;
-    private String s3;
+/**
+    s1 + c or s2 + c
+    dp[i][j] = (c in s1) dp[i - 1][j] or (c in s2) dp[i][j - 1]
 
+            d   b   b   c   a
+        1   0   
+    a   1   0   
+    a   1   1   1   1   1   0
+    b   0   1            
+    c                       
+    c                   
+
+    dp[dbbca][aabc]     add c to s1
+    dp[dbbc][aabcc]     add a to s2
+ */
+
+class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         if (s1.length() + s2.length() != s3.length()) return false;
-        this.s1 = s1;
-        this.s2 = s2;
-        this.s3 = s3;
-        dp = new HashMap<>();
-        return dfs(0, 0, 0);
-    }
 
-    private boolean dfs(int i, int j, int k) {
-        if (k == s3.length()) return true;
-
-        String state = i + "," + j;
-        if (dp.containsKey(state)) {
-            return dp.get(state);
+        int[] dp = new int[s2.length() + 1];
+        Arrays.fill(dp, 0);
+        dp[0] = 1;
+        for (int j = 1; j < dp.length; j++) {
+            if (s2.charAt(j - 1) == s3.charAt(j - 1)) {
+                dp[j] = 1;
+            } else {
+                break;
+            }
         }
 
-        if (i == s1.length()) return s2.substring(j).equals(s3.substring(k));
-        if (j == s2.length()) return s1.substring(i).equals(s3.substring(k));
-
-        boolean valid = false;
-        if (s1.charAt(i) == s3.charAt(k) && dfs(i + 1, j, k + 1)) valid = true;
-        if (s2.charAt(j) == s3.charAt(k) && dfs(i, j + 1, k + 1)) valid = true;
-
-        dp.put(state, valid);
-        return valid;
+        boolean s1_matching = true;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1_matching && s1.charAt(i) == s3.charAt(i)) {
+                dp[0] = 1;
+            } else {
+                dp[0] = 0;
+                s1_matching = false;
+            }
+            for (int j = 1; j < dp.length; j++) {
+                if (dp[j] == 1 && s1.charAt(i) == s3.charAt(i + j)) {
+                    dp[j] = 1;
+                } else if (dp[j - 1] == 1 && s2.charAt(j - 1) == s3.charAt(i + j)) {
+                    dp[j] = 1;
+                } else {
+                    dp[j] = 0;
+                }
+            }
+        }
+        return dp[dp.length - 1] == 1;
     }
 }
