380eab2922ad0e4b0b21f771044027fca57037df
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-03-22 00:10:40 -0400

    Time: 12 ms (92.81%) | Memory: 35.3 MB (53.78%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -3,9 +3,7 @@ class Solution:
         if min(nums1) & 1 == 1:
             return True
 
-        num_odd, num_even = 0, 0
         for num in nums1:
-            num_odd += 1 if num & 1 == 1 else 0
-            num_even += 1 if num & 1 == 0 else 0
-
-        return num_odd == len(nums1) or num_even == len(nums1)
+            if num & 1 == 1:
+                return False
+        return True
