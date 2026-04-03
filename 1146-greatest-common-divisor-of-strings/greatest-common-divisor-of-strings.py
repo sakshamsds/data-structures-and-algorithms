cb854e386d1ecb841e6cbd046638cc908048559b
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-04-03 17:51:38 -0400

    Time: 3 ms (24.17%) | Memory: 19.3 MB (82.56%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,6 +1,16 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1 + str2 != str2 + str1:
-            return ""
+        m, n = len(str1), len(str2)
+        cur = ""
+        res = cur
+        for i in range(min(m, n)):
+            cur += str1[i]
+            # print(cur)
 
-        return str1[:gcd(len(str1), len(str2))]
+            if m % (i + 1) != 0 or n % (i + 1) != 0:
+                continue
+
+            if cur * (m // (i + 1)) == str1 and cur * (n // (i + 1)) == str2:
+                res = cur
+
+        return res
