0bd0a3deda2c63243116ee1000d46b0040bdc543
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-05-09 19:55:37 -0400

    Time: 619 ms (57.23%) | Memory: 19.4 MB (57.23%) - LeetSync

--- 2426-maximum-profit-from-trading-stocks/maximum-profit-from-trading-stocks.py
+++ 2426-maximum-profit-from-trading-stocks/maximum-profit-from-trading-stocks.py
@@ -1,10 +1,7 @@
 class Solution:
     def maximumProfit(self, present: List[int], future: List[int], budget: int) -> int:
-        # 0-1 knapsack
         dp = [0] * (budget + 1)
         for p, f in zip(present, future):
             for b in range(budget, p - 1, -1):
                 dp[b] = max(dp[b], dp[b - p] + f - p)
-            # print(dp)
-
-        return dp[-1]
+        return dp[budget]
