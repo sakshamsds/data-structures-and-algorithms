b2464fb48a87c0d1548847774a58186a89510548
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-05-12 00:03:18 -0400

    Time: 10 ms (92.74%) | Memory: 43.8 MB (88.40%) - LeetSync

--- 1203-print-in-order/print-in-order.java
+++ 1203-print-in-order/print-in-order.java
@@ -1,24 +1,24 @@
 class Foo {
 
-    private Semaphore sem2 = new Semaphore(0);
-    private Semaphore sem3 = new Semaphore(0);
+    private CountDownLatch latch1 = new CountDownLatch(1);
+    private CountDownLatch latch2 = new CountDownLatch(1);
 
     public Foo() {
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
         printFirst.run();
-        sem2.release();
+        latch1.countDown();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        sem2.acquire();
+        latch1.await();
         printSecond.run();
-        sem3.release();
+        latch2.countDown();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        sem3.acquire();
+        latch2.await();
         printThird.run();
     }
 }
