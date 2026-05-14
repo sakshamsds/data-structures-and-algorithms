461ee8e8564ad9b52f819a1372b42cf934766983
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-05-14 19:58:29 -0400

    Time: 171 ms (7.25%) | Memory: 109.4 MB (8.41%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,45 +1,22 @@
 class TimeMap {
 
-    /*
-     key -> [(timestamp, value)]
-
-        k1 -> [(t1, v1), (t2, v2)]
-        k2 -> [(t3, v3), (t4, v4)]
-    */
-
-    private Map<String, List<Pair<Integer, String>>> store;
+    private Map<String, TreeMap<Integer, String>> map;
 
     public TimeMap() {
-        store = new HashMap<>(); 
+        map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        if (!store.containsKey(key)) {
-            store.put(key, new ArrayList<>());
-        }
-        store.get(key).add(new Pair(timestamp, value));
+        map.putIfAbsent(key, new TreeMap<>());
+        map.get(key).put(timestamp, value);        
     }
     
     public String get(String key, int timestamp) {
-        if (!store.containsKey(key)) {
-            return "";
-        }
-        int left = 0, right = store.get(key).size();
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int timestampPrev = store.get(key).get(mid).getKey();
-            if (timestampPrev <= timestamp) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-
-        if (left == 0) {
-            return "";
-        }
-        return store.get(key).get(left - 1).getValue();
+        if (!map.containsKey(key)) return "";
+        Integer floor = map.get(key).floorKey(timestamp);
+        // ceilingEntry O(logn)
+        if(floor == null) return "";
+        return map.get(key).get(floor);
     }
 }
 
