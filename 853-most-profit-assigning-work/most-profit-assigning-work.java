94b2eeefb536779e2b0d67433da38f47bb41c166
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-05-16 13:59:29 -0400

    Time: 26 ms (38.24%) | Memory: 48.2 MB (74.51%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -2,28 +2,22 @@ class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         // sort (difficulty, profit)
         List<int[]> jobProfile = new ArrayList<>();
-        jobProfile.add(new int[] {0, 0});
         for (int i = 0; i < difficulty.length; i++) {
             jobProfile.add(new int[] {difficulty[i], profit[i]});
         }
         Collections.sort(jobProfile, (a, b) -> Integer.compare(a[0], b[0]));
-
-        for (int i = 1; i < jobProfile.size(); i++) {
-            jobProfile.get(i)[1] = Math.max(jobProfile.get(i - 1)[1], jobProfile.get(i)[1]);
-        }
+        Arrays.sort(worker);
 
         int totalProfit = 0;
+        int ptr = 0;
+        int maxProfit = 0;
+
         for (int ability : worker) {
-            int left = 0, right = jobProfile.size();
-            while (left < right) {
-                int mid = left + (right - left) / 2;
-                if (ability < jobProfile.get(mid)[0]) {
-                    right = mid;
-                } else {
-                    left = mid + 1;
-                }
+            while (ptr < jobProfile.size() && ability >= jobProfile.get(ptr)[0]) {
+                maxProfit = Math.max(maxProfit, jobProfile.get(ptr)[1]);
+                ptr++;
             }
-            totalProfit += jobProfile.get(left - 1)[1];
+            totalProfit += maxProfit;
         }
         return totalProfit;
     }
