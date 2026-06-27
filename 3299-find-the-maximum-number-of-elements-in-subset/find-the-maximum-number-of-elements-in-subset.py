888334052304fdb041c74ec4c3f78a98bf5d2a19
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-06-27 19:32:51 -0400

    Time: 87 ms (98.40%) | Memory: 31.9 MB (25.60%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.py
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.py
@@ -1,29 +1,19 @@
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
-        dp = dict()         # start -> maxLength
         freqs = collections.Counter(nums)
 
         # handle edge case
-        maxOnes = freqs[1] if freqs[1] & 1 == 1 else freqs[1] - 1
+        maxLength = freqs[1] - 1 + (freqs[1] & 1)
         freqs.pop(1, None)
-        dp[1] = maxOnes
-
-        # print (freqs)
+        
         for start, freq in freqs.items():
-            # print(start, freq)
-            if freq == 1:
-                dp[start] = 1
-                continue
-
-            length = 1
-            nxt = start * start
-            while nxt in freqs:
-                length += 1
-                if freqs[nxt] == 1:
+            length = 0
+            while start in freqs:
+                if freqs[start] == 1:
+                    length += 1
                     break
-                nxt *= nxt
+                length += 2
+                start *= start
+            maxLength = max(maxLength, length - 1 + (length & 1))
 
-            dp[start] = 2 * length - 1
-
-        # print(dp)
-        return max(dp.values())
+        return maxLength
