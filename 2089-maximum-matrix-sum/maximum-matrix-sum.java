41e9c01001a7b8b52ea06611bec6da8dc6182e89
Sakshamdeep Singh <saksham.sds@gmail.com>
2026-09-16 14:58:37 -0400

    Time: 6 ms (84.59%) | Memory: 68.1 MB (47.21%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -3,12 +3,11 @@ class Solution {
         // migrate the negative sign
         int n = matrix.length;
         int num_neg = 0;
-        long total = 0;
+        long total = 0L;
         int min_abs_value = Integer.MAX_VALUE;
 
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                int num = matrix[r][c];
+        for (int[] row : matrix) {
+            for (int num : row) {
                 if (num < 0) num_neg++;
                 min_abs_value = Math.min(min_abs_value, Math.abs(num));
                 total += Math.abs(num);
